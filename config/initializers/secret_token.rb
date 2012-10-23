90e8dc73397073071b97c5ed4aed61618db51fdc
Hung Doan <hdoan741@gmail.com>
2012-10-23 22:15:49 +0800

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JfdiAcademy::Application.config.secret_token = '4f9aebeee82237ccbaa27c8691c073c52cb1a9b823c371d6564dfac801778536476cdae6d8e075050e32dbc8d1d5a7a8b30048db204a9c14022b1e0a3ddb09a9'
