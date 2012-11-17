ee943f7f6be10b25046e72b09b82d7598fe103df
Hung Doan <hdoan741@gmail.com>
2012-11-17 14:19:45 +0800

    Update view for announcement.

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,5 +1,5 @@
 class Announcement < ActiveRecord::Base
-  attr_accessible :course_id, :creator_id, :important, :publish_at
+  attr_accessible :course_id, :creator_id, :description, :important, :publish_at, :title
 
   belongs_to :course
   belongs_to :creator, class_name: "User"
