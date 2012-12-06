179a3268096a4d688bd9523909ea57965687b722
Hung Doan <hdoan741@gmail.com>
2012-12-06 20:27:06 +0800

    Update db model for answer.

--- app/models/student_answer.rb
+++ app/models/student_answer.rb
@@ -1,6 +1,6 @@
 class StudentAnswer < ActiveRecord::Base
-  attr_accessible :answer_id, :note, :started_at, :submitted_at
+  attr_accessible :answer_id, :note, :started_at, :student_id, :submitted_at, :text
 
   belongs_to :answer
-  belongs_to :creator, through: :answer
+  belongs_to :student, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117052103) do
+ActiveRecord::Schema.define(:version => 20121206122108) do
 
   create_table "announcements", :force => true do |t|
     t.integer  "creator_id"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(:version => 20121117052103) do
     t.string   "note"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "student_id"
+    t.string   "text"
   end
 
   create_table "user_courses", :force => true do |t|
