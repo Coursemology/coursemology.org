0e0a2f78f5b3269a97ac771a473447a8dad1d044
Hung Doan <hdoan741@gmail.com>
2012-12-16 16:23:56 +0800

    add requirements to title, reward and mission (model only.)

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,4 +2,7 @@ class Reward < ActiveRecord::Base
   attr_accessible :course_id, :creator_id, :description, :icon_url, :title
 
   belongs_to :course
+
+  has_many :requirements, as: :obj
+  has_many :ach_reqs, through: :requirements, source: :req, source_type: "Achievement"
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -2,4 +2,8 @@ class Title < ActiveRecord::Base
   attr_accessible :course_id, :creator_id, :description, :title
 
   belongs_to :course
+
+  has_many :requirements, as: :obj
+  has_many :ach_reqs, through: :requirements, source: :req, source_type: "Achievement"
+  has_many :lvl_reqs, through: :requirements, source: :req, source_type: "Level"
 end
