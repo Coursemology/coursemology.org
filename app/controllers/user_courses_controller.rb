783390ae8f46f61a0f529636c81d9cf31a82184d
Hung Doan <hdoan741@gmail.com>
2013-02-10 22:02:10 +0700

    fix achievement icons in user page. split tags by group.

--- app/controllers/user_courses_controller.rb
+++ app/controllers/user_courses_controller.rb
@@ -6,5 +6,6 @@ class UserCoursesController < ApplicationController
 
   def show
     @user_course.create_all_std_tags
+    @std_tags = @user_course.std_tags.sort_by { |std_tag| std_tag.tag.tag_group_id || 0 }
   end
 end
