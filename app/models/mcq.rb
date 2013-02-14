7a222e0c9ee0f26039bcc4fe472b140d2d1782e4
Hung Doan <hdoan741@gmail.com>
2013-02-14 09:56:33 +0700

    can delete questions now.

--- app/models/mcq.rb
+++ app/models/mcq.rb
@@ -5,5 +5,5 @@ class Mcq < ActiveRecord::Base
 
   has_many :mcq_answers, dependent: :destroy
   has_many :std_mcq_answers, dependent: :destroy
-  has_many :asm_qns, dependent: :destroy
+  has_many :asm_qns, as: :qn, dependent: :destroy
 end
