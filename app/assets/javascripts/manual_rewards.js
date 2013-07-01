d3dc76f5ac1c7a7e3537afc2b72ce09aafb6a340
Hung Doan <hdoan741@gmail.com>
2013-07-01 13:19:38 +0700

    improve EXP giveaway.

--- app/assets/javascripts/manual_rewards.js
+++ app/assets/javascripts/manual_rewards.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+  $('.repeat-exp-link').click(function(evt) {
+    evt.preventDefault();
+    var exp = $(this).parent().children('input').val();
+    $('.exp-input').val(exp);
+  });
+});
