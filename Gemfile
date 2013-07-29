f207528fa6b1bf45d29d3b0590547b2cd3265d14
Raymond <raymondtangsc@gmail.com>
2013-07-29 16:42:39 +0800

    added newrelic to monitor performance

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.11'
 
 group :development do
   gem 'sqlite3'
+  gem 'newrelic_rpm'
 end
 
 group :production do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ JfdiAcademy::Application.configure do
   # Setup mailer
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
