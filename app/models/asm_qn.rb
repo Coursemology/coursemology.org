56d9bee4cadcdfac4495e0759cb3f990ce2b5c07
Raymond <raymondtangsc@gmail.com>
2013-07-31 17:06:12 +0800

    delete questions when mission is deleted

--- app/models/asm_qn.rb
+++ app/models/asm_qn.rb
@@ -2,5 +2,5 @@ class AsmQn < ActiveRecord::Base
   attr_accessible :asm_id, :asm_type, :qn_id, :qn_type, :pos
 
   belongs_to :asm, polymorphic: true
-  belongs_to :qn, polymorphic: true
+  belongs_to :qn, polymorphic: true, dependent: :destroy
 end
