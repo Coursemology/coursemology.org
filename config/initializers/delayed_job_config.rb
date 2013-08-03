c823368547854c8c9f2d87938d79dcd52eaf0dd3
Raymond <raymondtangsc@gmail.com>
2013-08-03 12:18:27 +0800

    delayed job config

---
+++ config/initializers/delayed_job_config.rb
@@ -0,0 +1,8 @@
+# config/initializers/delayed_job_config.rb
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.delay_jobs = !Rails.env.test?
