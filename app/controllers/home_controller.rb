33c4329403b38d629874adcb5eeea11ed063a20f
Raymond <raymondtangsc@gmail.com>
2013-08-06 00:52:21 +0800

    auto direct student to course if he is only attending one course

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
 
   def index
     @courses = current_user.courses
+
+    if @courses.count == 1
+      user_course = UserCourse.find_by_user_id_and_course_id(current_user, @courses.first)
+      if user_course.is_student?
+        redirect_to course_path(@courses.first)
+      end
+    end
+
     @courses_std = current_user.user_courses.student.map { |uc| uc.course }
     @courses_staff = current_user.user_courses.staff.map { |uc| uc.course }
     @courses_shared = current_user.user_courses.shared.map { |uc| uc.course }
