d6ee7dc7b38e8e50b70e8f30bf4a2347ee025996
Raymond <raymondtangsc@gmail.com>
2013-08-06 01:21:34 +0800

    enable edit email in manage staff

--- app/assets/javascripts/user_courses.js
+++ app/assets/javascripts/user_courses.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
 
     $(document).on('click', '.remove-staff', function(e) {
         e.preventDefault();
-        if(confirm('Are you sure you want to delete this staff?')) {
-            var staff_row = $(this).parents('tr');
-            var url = staff_row.find('.uc-url').attr('href');
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function() {
-                    staff_row.remove();
-                }
-            });
-        }
+        var staff_row = $(this).parents('tr');
+        var url = staff_row.find('.uc-url').attr('href');
+        $.ajax({
+            url: url,
+            type: 'DELETE',
+            success: function() {
+                staff_row.remove();
+            }
+        });
     });
 
 //    $(document).on('change','.change-role',function(e){
@@ -41,29 +39,39 @@ $(document).ready(function() {
 //        }
 //    });
 
+    function showAlertNotice(str) {
+        var notice = $('.alert');
+        notice.slideDown();
+        notice._removeClass('hidden');
+        notice.removeClass("alert-success");
+        notice.addClass("alert-error");
+        notice.text(str);
+        notice.slideDown(function(){
+            setTimeout(function(){
+                notice.slideUp()
+            },1500);
+        });
+
+    }
     $('.update-staff').on('click',function(e) {
         e.preventDefault();
         var staff_row = $(this).parents('tr');
         var curr_role_val = staff_row.find('.change-role').val();
         var name_field = staff_row.find('.change-name');
+        var email_field = staff_row.find('.change-email');
         var old_name  = staff_row.find('.old-name');
         var notice = $('.alert');
         if(notice.size() > 1) {
             notice[0].parentNode.removeChild(notice[0]);
         }
         if(name_field.val().length == 0) {
-            notice.slideDown();
-            notice._removeClass('hidden');
-            notice.removeClass("alert-success");
-            notice.addClass("alert-error");
-            notice.text("User name can't be empty!");
-            notice.slideDown(function(){
-                setTimeout(function(){
-                    notice.slideUp()
-                },1500);
-            });
+            showAlertNotice("User name can't be empty!");
             name_field.val(old_name.val());
             return;
+        }
+        if((typeof email_field != 'undefined') && !email_field[0].validity.valid) {
+            showAlertNotice("Please input valid email address!");
+            return
         }
         old_name.val(name_field.val());
         var url = staff_row.find('.uc-url').attr('href');
@@ -77,7 +85,8 @@ $(document).ready(function() {
             url: url,
             type: 'PUT',
             data: { role_id: curr_role_val,
-                name:name_field.val()},
+                name:name_field.val(),
+                email: email_field.val() },
             dataType: 'html',
             success: function() {
                 notice.slideDown(function(){
