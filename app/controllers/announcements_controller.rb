2f2280ffd8242867c834279f70f738c135dd611d
Raymond <raymondtangsc@gmail.com>
2013-08-14 14:20:10 +0800

    announcement publish default to current time

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -19,7 +19,7 @@ class AnnouncementsController < ApplicationController
   end
 
   def new
-    @announcement.publish_at = DateTime.now.beginning_of_day
+    @announcement.publish_at = DateTime.now
   end
 
   def edit
