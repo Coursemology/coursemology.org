1236aae52785a2b2e96fddb3013548d278a60aa3
Hung Doan <hdoan741@gmail.com>
2013-08-23 15:47:50 +0700

    update leaderboard ranking.

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -7,7 +7,11 @@ class LeaderboardsController < ApplicationController
 
     top_pref = @course.leaderboard_no_pef.prefer_value.to_i
     student_courses = @course.student_courses
-    @top_exp = student_courses.student.where(is_phantom: false).order('exp DESC, exp_updated_at ASC').first(top_pref)
-    @top_ach = student_courses.student.where(is_phantom: false).order('exp_updated_at ASC').sort_by { |sc| sc.user_achievements.count }.reverse.first(top_pref)
+    @top_exp = student_courses.student.where(is_phantom: false)
+        .order('level_id DESC, exp DESC, exp_updated_at ASC, id ASC')
+        .first(top_pref)
+    @top_ach = student_courses.student.where(is_phantom: false)
+        .order('exp_updated_at ASC, id ASC')
+        .sort_by { |sc| sc.user_achievements.count }.reverse.first(top_pref)
   end
 end
