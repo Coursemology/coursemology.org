f90013dce2a73511d0aee1110364917bd3f5d504
Raymond <raymondtangsc@gmail.com>
2013-08-25 03:50:22 +0800

    test python sand box

--- app/helpers/training_submissions_helper.rb
+++ app/helpers/training_submissions_helper.rb
@@ -6,22 +6,15 @@ module TrainingSubmissionsHelper
     rand_str = ''
     begin
       rand_str = (0...20).map{ ('a'..'z').to_a[rand(26)] }.join
-      full_path = @@path_temp_folder + rand_str
+      full_path = @@path_temp_folder + rand_str + '.py'
     end while File.exist?(full_path)
 
     full_path
   end
 
   def get_code_to_write(included_code,code_to_run)
-    code_to_write =
-        "#################
-# Include code  #
-#################
-"<< included_code << "
-#################
-#  student code #
-#################
-"<< code_to_run;
+    code_to_run  << '
+' << included_code
 
   end
 
@@ -61,11 +54,13 @@ module TrainingSubmissionsHelper
         #}
         @stdin,@stdout,@stderr = Open3.popen3("python3 #{file_path}")
         errors = @stderr.readlines
-        results = @stdout.readlines.map{|r| if r.gsub("\n",'') == "True" then true else false end}
+        stdout = @stdout.readlines
+        results = stdout.map{|r| if r.gsub("\n",'') == "True" then true else false end}
         @stdin.close
         @stderr.close
         @stdout.close
         File.delete(file_path)
+
 
         test_type = if i == 0 then :publicTests else :privateTests end
         summary[test_type] = results
@@ -74,6 +69,59 @@ module TrainingSubmissionsHelper
           summary[:errors] = error_message.join
           break
         end
+
+      end
+    end
+    puts ">>>>>>>>>>>>", summary
+    summary
+  end
+
+  def eval_python2(file_path, code, data)
+    tests = {publicTests:data["publicTests"],
+             privateTests:data["privateTests"]}
+    timeLimit = data["timeLimitInSec"]
+    memoryLimit = data["memoryLimitInMB"]
+    FileUtils.mkdir_p(@@path_temp_folder) unless File.exist?(@@path_temp_folder)
+    summary ={publicTests:[],privateTests:[],errors:[]}
+    for i in 0..1
+      file = File.open(file_path, 'w+')
+      if file
+        file.write(code)
+        case i
+          when 0
+            test_type = tests[:publicTests]
+          when 1
+            test_type = tests[:privateTests]
+        end
+        test_code = ''
+        test_type.each do |test|
+          test_code << "\nprint(#{test["expression"]} == #{test["expected"]})\n"
+        end
+        file.write(test_code)
+        file.close
+
+        @stdin,@stdout,@stderr = Open3.popen3("python3 #{@@path_temp_folder}#{"exec.py -t "<< timeLimit << " -m "<<memoryLimit << " "<< file_path}")
+        output = @stdout.readlines
+        @stdin.close
+        @stderr.close
+        @stdout.close
+        File.delete(file_path)
+
+        if output.size == 1
+          summary[:errors] = "Your solution was rejected as it exceeded the time limit for this question."
+          break
+        end
+
+        trace = JSON.parse(output.join)["trace"]
+
+        if trace["event"] != "return"
+          summary[:errors] = trace["event"] << "\n  " << trace["exception_msg"]
+          break
+        end
+
+        results = trace["stdout"].split("\n")
+        test_type = if i == 0 then :publicTests else :privateTests end
+        summary[test_type] = results.map {|r| if r == 'False' then false else true end }
 
       end
     end
