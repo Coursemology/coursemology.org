e31544d636c6a00b0f63be69f5b0491d74117f1b
Hung Doan <hdoan741@gmail.com>
2013-09-01 13:19:24 +0700

    tighten question re ordering.

--- app/controllers/asm_qns_controller.rb
+++ app/controllers/asm_qns_controller.rb
@@ -1,10 +1,24 @@
 class AsmQnsController < ApplicationController
+  load_and_authorize_resource :course
+
+  before_filter :load_asm, only: [:reorder]
+
+  def load_asm
+    if params[:training_id]
+      @asm = @course.trainings.find(params[:training_id])
+    else
+      @asm = @course.missions.find(params[:mission_id])
+    end
+    authorize! :edit, @asm
+  end
 
   def reorder
     # binding.pry
     params['asm-qn'].each_with_index do |id, index|
       # binding.pry
-      AsmQn.update_all("pos = #{index}", "id = #{id}");
+      asm_qn = @asm.asm_qns.find(id.to_i)
+      asm_qn.pos = index
+      asm_qn.save
     end
     # binding.pry
 
