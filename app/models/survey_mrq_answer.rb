839f2164c49abbada5edc64f20e87647059dbd9c
Raymond <raymondtangsc@gmail.com>
2013-09-13 18:14:25 +0800

    add back options for survey answer

--- app/models/survey_mrq_answer.rb
+++ app/models/survey_mrq_answer.rb
@@ -1,9 +1,14 @@
 class SurveyMrqAnswer < ActiveRecord::Base
   acts_as_paranoid
-  attr_accessible :user_course_id, :question_id, :option_id
+
+  attr_accessible :user_course_id, :question_id, :option_id, :selected_options
 
   belongs_to :user_course
   belongs_to :question, class_name: "SurveyQuestion"
   belongs_to :option, class_name: "SurveyQuestionOption"
+
+  def options
+    SurveyQuestionOption.where(id: eval(selected_options))
+  end
 
 end
