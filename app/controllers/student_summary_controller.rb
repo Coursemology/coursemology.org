8f54924c8df0b9c7c0608c9c910fc1dc4eca2734
Raymond <raymondtangsc@gmail.com>
2013-09-16 13:07:02 +0800

    student summary for phantom students

--- app/controllers/student_summary_controller.rb
+++ app/controllers/student_summary_controller.rb
@@ -16,10 +16,13 @@ class StudentSummaryController < ApplicationController
         @students = @course.user_courses.student.where(is_phantom: false).order("exp desc")
     end
 
+
     @students = @students.includes(:exp_transactions)
     @std_summary_paging = @course.std_summary_paging_pref
     if @std_summary_paging.display?
       @students = @students.page(params[:page]).per(@std_summary_paging.prefer_value.to_i)
     end
+
+    @phantom_students = @course.user_courses.student.where(is_phantom: true).order("exp desc")
   end
 end
