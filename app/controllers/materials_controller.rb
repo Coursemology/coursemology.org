ea60aeaefa288ae19983f9d6fcd66b06d1d43d57
Joel Low <joel@joelsplace.sg>
2013-10-19 16:06:20 +0800

    Show and index for the materials controller should give a folder listing.

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,13 +1,16 @@
 class MaterialsController < ApplicationController
   load_and_authorize_resource :course
 
-  def index(id = nil)
+  def index
+    show nil
+  end
+  
+  def show id
     @folder = if id then
                 MaterialFolder.find_by_id(id)
               else
                 MaterialFolder.find_by_course_id_and_parent_folder_id(@course.id, nil)
               end
-    
   end
 
   def new(parent)
