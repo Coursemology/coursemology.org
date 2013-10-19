19e9156be1a8f08ea7d8ece44707cc2dab8ff1ac
Joel Low <joel@joelsplace.sg>
2013-10-19 10:33:56 +0800

    Sorry, syntax errors while reformatting.

--- db/migrate/20131019100355_create_materials.rb
+++ db/migrate/20131019100355_create_materials.rb
@@ -1,12 +1,14 @@
 class CreateMaterials < ActiveRecord::Migration
   def change
     create table :material_folders do |t|
-    t.integer :parent_folder_id
-    t.integer :course_id
-    t.string  :name
-    t.text    :description
-  end
-  create table :materials do |t
-    t.string  :name
+      t.integer :parent_folder_id, default: nil
+      t.integer :course_id
+      t.string  :name
+      t.text    :description
+    end
+    create table :materials do |t|
+      t.integer :folder_id
+      t.text    :description
+    end
   end
 end
