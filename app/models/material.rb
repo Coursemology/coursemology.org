fdde39335ca2449d7d170eb2bf7b7c7ccf846536
Joel Low <joel@joelsplace.sg>
2013-10-19 17:45:59 +0800

    Implement display of the real file name of an uploaded file.

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,9 @@
 class Material < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :folder, class_name: "MaterialFolder"
+  has_one :file, as: :owner, class_name: "FileUpload", dependent: :destroy
+  
+  def filename
+    self.file.original_name || self.file.file_file_name
+  end
 end
