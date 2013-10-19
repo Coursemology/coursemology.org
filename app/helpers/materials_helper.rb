7ee3d27326b8f266987cc17396093c2069a2b79d
Jerome Cheng <jcheng31@gmail.com>
2013-10-19 10:42:55 +0000

    Add MaterialsHelper file, code to get file size prefix.

---
+++ app/helpers/materials_helper.rb
@@ -0,0 +1,12 @@
+module MaterialsHelper
+  def get_file_size_suffix(size_in_bytes)
+    suffixes = ["B", "KB", "MB", "GB"]
+    size = size_in_bytes
+    i = 0
+    while size > 1 and i < 4 do
+      size = size / 1024
+      i += 1
+    end
+    suffixes[i]
+  end
+end
