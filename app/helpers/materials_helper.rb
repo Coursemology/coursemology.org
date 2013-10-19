8751b5dc7c39263895c2e06f5c794a3966e41434
Joel Low <joel@joelsplace.sg>
2013-10-19 21:11:02 +0800

    Fix the file size formatting function.

--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,12 +1,12 @@
 module MaterialsHelper
-  def get_file_size_suffix(size_in_bytes)
+  def human_file_size(size_in_bytes)
     suffixes = ["B", "KB", "MB", "GB"]
     size = size_in_bytes
     i = 0
-    while size > 1 and i < suffixes.length do
-      size = size / 1024
+    while size > 1024 and i < suffixes.length do
+      size /= 1024
       i += 1
     end
-    suffixes[i]
+    "#{size} #{suffixes[i]}"
   end
 end
