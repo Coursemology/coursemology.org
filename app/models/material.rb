c99a9b5e2b05ac605033d4bf7496d4157039ea6d
Joel Low <joel@joelsplace.sg>
2013-10-20 20:31:25 +0800

    Implemented materials renaming and description updating.

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,10 +3,19 @@ class Material < ActiveRecord::Base
   belongs_to :folder, class_name: "MaterialFolder"
   has_one :file, as: :owner, class_name: "FileUpload", dependent: :destroy
 
-  attr_accessible :folder
+  attr_accessible :folder, :description, :filename
+  after_save :save_file
+
+  def save_file
+    self.file.save
+  end
 
   def filename
     self.file.original_name || self.file.file_file_name
+  end
+  
+  def filename=(filename)
+    self.file.original_name = filename
   end
 
   def filesize
