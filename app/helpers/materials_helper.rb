121c947bf5c1e5def1df6e2df2304bf3140385a0
Joel Low <joel@joelsplace.sg>
2013-10-25 18:37:32 +0800

    Fix file size units.

--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,12 +1,14 @@
 module MaterialsHelper
-  def human_file_size(size_in_bytes)
+  def get_pretty_file_size(size_in_bytes)
     suffixes = ["B", "KB", "MB", "GB"]
     size = size_in_bytes
-    i = 0
-    while size > 1024 and i < suffixes.length do
-      size /= 1024
-      i += 1
+    size_current_units = size_in_bytes
+    unit = 0
+    while size_current_units > 1024 and unit < suffixes.length do
+      size_current_units = size_current_units / 1024.0
+      unit += 1
     end
-    "#{size} #{suffixes[i]}"
+    size_rounded = sprintf "%.2f", size_current_units
+    size_rounded + " " + suffixes[unit]
   end
 end
