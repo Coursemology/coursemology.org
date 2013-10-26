f62488438e82a3ad7f98237b78f4471ec08445ff
Joel Low <joel@joelsplace.sg>
2013-10-26 15:14:29 +0800

    Implemented editing milestones.

--- app/controllers/lesson_plan_milestones_controller.rb
+++ app/controllers/lesson_plan_milestones_controller.rb
@@ -25,6 +25,21 @@ class LessonPlanMilestonesController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    respond_to do |format|
+      if @lesson_plan_milestone.update_attributes(params[:lesson_plan_milestone]) && @lesson_plan_milestone.save then
+        format.html { redirect_to course_lesson_plan_milestone_path(@course, @lesson_plan_milestone),
+                      notice: "The lesson plan milestone #{@lesson_plan_milestone.title} has been updated." }
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
 private
   def render(*args)
     options = args.extract_options!
