b6ba291527d53bcd8621c6f1ffe3203dafebb52b
Jerome Cheng <jcheng31@gmail.com>
2013-10-27 12:48:36 +0000

    Fix context issues with node-click handler.

--- app/assets/javascripts/materials_file_picker.js
+++ app/assets/javascripts/materials_file_picker.js
@@ -52,34 +52,38 @@ MaterialsFilePicker.prototype.onWorkbinStructureReceived = function(rootNode) {
   this.treeElement.tree({
     data: treeData,
     autoOpen: true,
-    keyboardSupport: false    
+    keyboardSupport: false
   });
+  this.treeElement.bind('tree.click', this.onNodeClicked(this));
 };
 
-MaterialsFilePicker.prototype.onNodeClicked = function(event) {
-  // Disable single selection: click to select for everything.
-  event.preventDefault();
-  
-  var selectedNode = event.node;
-  var nodeId = selectedNode.id;
-  
-  var isNodeSelected = this.treeElement.tree('isNodeSelected', selectedNode);
-  var isNodeAFile = nodeId.indexOf("file") !== -1;
-  
-  // We don't bother with folders - only individual files.
-  if (isNodeAFile) {
-    var indexAfterPrefix = nodeId.indexOf("_") + 1;
-    var id = nodeId.slice(indexAfterPrefix);
+MaterialsFilePicker.prototype.onNodeClicked = function(context) {
+  var that = context;
+  return function(event) {
+    // Disable single selection: click to select for everything.
+    event.preventDefault();
     
-    // <ID, Type, Name, URL>
-    if (isNodeSelected) {
-      this.treeElement.tree('removeFromSelection', selectedNode);
+    var selectedNode = event.node;
+    var nodeId = selectedNode.id;
+    
+    var isNodeSelected = context.treeElement.tree('isNodeSelected', selectedNode);
+    var isNodeAFile = nodeId.indexOf && nodeId.indexOf("file") !== -1;
+    
+    // We don't bother with folders - only individual files.
+    if (isNodeAFile) {
+      var indexAfterPrefix = nodeId.indexOf("_") + 1;
+      var id = nodeId.slice(indexAfterPrefix);
       
-      var tuple = [id, "Material", selectedNode.label, selectedNode.url];
-      this.selectedMaterials[id] = tuple;
-    } else {
-      this.treeElement.tree('addToSelection', selectedNode);
-      delete this.selectedMaterials[id];
+      // <ID, Type, Name, URL>
+      if (isNodeSelected) {
+        context.treeElement.tree('removeFromSelection', selectedNode);
+        
+        var tuple = [id, "Material", selectedNode.label, selectedNode.url];
+        context.selectedMaterials[id] = tuple;
+      } else {
+        context.treeElement.tree('addToSelection', selectedNode);
+        delete context.selectedMaterials[id];
+      }
     }
-  }
+  };
 };
