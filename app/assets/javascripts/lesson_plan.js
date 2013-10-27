555b9a6b03493aeaf759fc03b055106394a5bc73
Joel Low <joel@joelsplace.sg>
2013-10-27 17:52:16 +0800

    Implemented UI on the form to link resources.

--- app/assets/javascripts/lesson_plan.js
+++ app/assets/javascripts/lesson_plan.js
@@ -1,9 +1,27 @@
-function LessonPlanEntryForm(pickers) {
+function LessonPlanEntryFormType(pickers) {
+  this.pickers = pickers;
   pickers.forEach(function(picker) {
     picker.onSelectionCompleted = this.doneCallback;
   });
 }
 
-LessonPlanEntryForm.prototype.doneCallback = function(idTypePairList) {
-  
+LessonPlanEntryFormType.prototype.pick = function() {
+  var $modal = $('<div class="modal hide fade" />');
+  this.pickers[0].pick($modal[0]);
+  $modal.modal();
+}
+
+LessonPlanEntryFormType.prototype.doneCallback = function(idTypePairList) {
+  idTypePairList.forEach(function(x) {
+    $("#uploaded_files_div").append(
+      '<input type="hidden" name="resources[]" value="' + x[0] + ',' + x[1] + '" />');
+  });
 };
+
+var LessonPlanEntryForm = new LessonPlanEntryFormType([]);
+
+$(document).ready(function() {
+  $('.addresource-button').click(function() {
+    LessonPlanEntryForm.pick();
+  });
+});
