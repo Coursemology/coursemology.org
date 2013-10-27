57c1faa544a82c3e2dd0a0b8f80f175532e5124d
Jerome Cheng <jcheng31@gmail.com>
2013-10-27 10:39:26 +0000

    Rough skeleton for material file picker.

--- app/assets/javascripts/materials_file_picker.js
+++ app/assets/javascripts/materials_file_picker.js
@@ -1,8 +1,19 @@
 function MaterialsFilePicker(callback) {
   this.doneCallback = callback;
   this.selectedMaterials = [];
+  
+  var courseId = gon.course;
+  var that = this;
+  $.ajax({
+    url: '/courses/' + courseId + '/materials.json',
+    success: that.onWorkbinStructureReceived
+  });
 }
 
 MaterialsFilePicker.prototype.onSelectionCompleted = function() {
   this.doneCallback(this.selectedMaterials);
+}
+
+MaterialsFilePicker.prototype.onWorkbinStructureReceived = function(rootNode) {
+  
 }
