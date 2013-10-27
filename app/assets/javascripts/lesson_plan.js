11a944cb7881db56137e826c3529f2ccca5c226e
Joel Low <joel@joelsplace.sg>
2013-10-27 19:23:49 +0800

    Define the lesson plan form type only on page load.

--- app/assets/javascripts/lesson_plan.js
+++ app/assets/javascripts/lesson_plan.js
@@ -1,34 +1,34 @@
-function LessonPlanEntryFormType(pickers) {
-  var self = this;
-  this.pickers = pickers;
-  pickers.forEach(function(picker) {
-    picker.onSelectionCompleted = function() { self.doneCallback.apply(self, arguments); }
-  });
-}
-
-LessonPlanEntryFormType.prototype.pick = function() {
-  var $modal = $('<div class="modal hide fade" />');
-  this.pickers[0].pick($modal[0]);
-  $modal.modal();
-}
-
-LessonPlanEntryFormType.prototype.doneCallback = function(idTypePairList) {
-  idTypePairList.forEach(function(x) {
-    $element = $('<tr>\n\
-         <td>' + x[2] + '</td>\n\
-         <td>&nbsp;</td>\n\
-         <td>\n\
-           <span class="btn btn-danger resource-delete"><i class="icon-trash"></i></span>\n\
-           <input type="hidden" name="resources[]" value="' + x[0] + ',' + x[1] + '" />\n\
-         </td>\n\
-       </tr>');
-    $("#linked_resources tbody").append($element);
-  });
-};
-
-var LessonPlanEntryForm = new LessonPlanEntryFormType([]);
-
 $(document).ready(function() {
+  function LessonPlanEntryFormType(pickers) {
+    var self = this;
+    this.pickers = pickers;
+    pickers.forEach(function(picker) {
+      picker.onSelectionCompleted = function() { self.doneCallback.apply(self, arguments); }
+    });
+  }
+
+  LessonPlanEntryFormType.prototype.pick = function() {
+    var $modal = $('<div class="modal hide fade" />');
+    this.pickers[0].pick($modal[0]);
+    $modal.modal();
+  }
+
+  LessonPlanEntryFormType.prototype.doneCallback = function(idTypePairList) {
+    idTypePairList.forEach(function(x) {
+      $element = $('<tr>\n\
+        <td>' + x[2] + '</td>\n\
+        <td>&nbsp;</td>\n\
+        <td>\n\
+          <span class="btn btn-danger resource-delete"><i class="icon-trash"></i></span>\n\
+          <input type="hidden" name="resources[]" value="' + x[0] + ',' + x[1] + '" />\n\
+        </td>\n\
+      </tr>');
+      $("#linked_resources tbody").append($element);
+    });
+  };
+
+  var LessonPlanEntryForm = new LessonPlanEntryFormType([]);
+
   $('.addresource-button').click(function() {
     LessonPlanEntryForm.pick();
   });
