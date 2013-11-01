7f863f474723336d679fe1e05355d7cb12d2191f
Fazli Sapuan <fazli@sapuan.org>
2013-11-02 01:11:40 +0800

    moderation tools fix

---
+++ app/decorator/controllers/forem/admin/topics_controller_decorator.rb
@@ -0,0 +1,13 @@
+Forem::Admin::TopicsController.class_eval do
+  #load_and_authorize_resource :course
+  #before_filter :shim
+
+  private
+
+  def shim
+    @category ||= Forem::Category.find(@course.id)
+    @current_ability = CourseAbility.new(current_user, curr_user_course)
+    load_general_course_data
+    @current_ability = Forem::Ability.new(forem_user)
+  end
+end
--- app/decorator/controllers/forem/moderation_controller_decorator.rb
+++ app/decorator/controllers/forem/moderation_controller_decorator.rb
@@ -1,29 +1,15 @@
 Forem::ModerationController.class_eval do
-  def index
-    @posts = forum.posts.pending_review.topic_not_pending_review
-    @topics = forum.topics.pending_review
-    @course = Course.find(@forum.category.id)
-    load_general_course_data
-  end
+  before_filter :shim
 
-  def posts
-    @course = Course.find(@forum.category.id)
-    load_general_course_data
-    Forem::Post.moderate!(params[:posts] || [])
-    flash[:notice] = t('forem.posts.moderation.success')
-    redirect_to :back
-  end
+  private
 
-  def topic
-    @course = Course.find(@forum.category.id)
+  def shim
+    #@category ||= Forem::Category.find(@course.id)
+    @forum ||= Forem::Forum.find(params[:forum_id])
+    @category = Forem::Category.find(@forum.category_id)
+    @course = Course.find(@category.id)
+    @current_ability = CourseAbility.new(current_user, curr_user_course)
     load_general_course_data
-    if params[:topic]
-      topic = forum.topics.find(params[:topic_id])
-      topic.moderate!(params[:topic][:moderation_option])
-      flash[:notice] = t("forem.topic.moderation.success")
-    else
-      flash[:error] = t("forem.topic.moderation.no_option_selected")
-    end
-    redirect_to :back
+    @current_ability = Forem::Ability.new(forem_user)
   end
 end
