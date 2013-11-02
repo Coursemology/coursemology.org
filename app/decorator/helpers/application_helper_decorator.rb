f74e79c75bf2fc3880b673839265bc6c0b1727cb
Minqi Chen <cnchenminqi@gmail.com>
2013-11-02 18:00:25 +0800

    Disable emoji for now

--- app/decorator/helpers/application_helper_decorator.rb
+++ app/decorator/helpers/application_helper_decorator.rb
@@ -4,12 +4,13 @@ Forem::ApplicationHelper.class_eval do
   end
 
   def forem_no_escape_emojify(content)
-    content.gsub(/:([a-z0-9\+\-_]+):/) do |match|
-      if Emoji.names.include?($1)
-        '<img alt="' + $1 + '" height="20" src="' + asset_path("emoji/#{$1}.png") + '" style="vertical-align:middle" width="20" />'
-      else
-        match
-      end
-    end.html_safe if content.present?
+    #content.gsub(/:([a-z0-9\+\-_]+):/) do |match|
+    #  if Emoji.names.include?($1)
+    #    '<img alt="' + $1 + '" height="20" src="' + asset_path("emoji/#{$1}.png") + '" style="vertical-align:middle" width="20" />'
+    #  else
+    #    match
+    #  end
+    #end.html_safe if content.present?
+    content
   end
 end
