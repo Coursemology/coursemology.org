f944dc894662d0576d8c453f4765a9271c536a70
Minqi Chen <cnchenminqi@gmail.com>
2013-11-02 23:18:13 +0800

    date range validation

--- app/controllers/forum_participation_controller.rb
+++ app/controllers/forum_participation_controller.rb
@@ -6,15 +6,23 @@ class ForumParticipationController < ApplicationController
   def manage
     @from_date = params[:from] || '01-10-2013'
     @to_date = params[:to] || '30-11-2013'
-    from_date_db = date_dmy_to_ymd(@from_date) + ' 00:00:00'
-    to_date_db = date_dmy_to_ymd(@to_date) + ' 23:59:59'
+    from_date_db = parse_start_date(@from_date)
+    to_date_db = parse_end_date(@to_date)
 
     @students_courses = @course.user_courses.student.order('lower(name)')
     category ||= Forem::Category.find(@course.id)
     result = Forem::Forum.
         joins(topics: :posts).
-        where('category_id = ? AND forem_posts.created_at >= ? AND forem_posts.created_at <= ?',
-              category.id, from_date_db, to_date_db).select('COUNT(*) as post_count, forem_posts.user_id').group('forem_posts.user_id')
+        where('category_id = ?', category.id)
+    if (from_date_db)
+      result.where('forem_posts.created_at >= ?', from_date_db)
+    end
+    if (to_date_db)
+      result.where('forem_posts.created_at <= ?', to_date_db)
+    end
+    result = result.
+        select('COUNT(*) as post_count, forem_posts.user_id').
+        group('forem_posts.user_id')
     @post_count = {}
     result.each {|i| @post_count[i.user_id] = i.post_count}
     @range_selection = {}
@@ -44,11 +52,26 @@ class ForumParticipationController < ApplicationController
   end
 
   private
-  def date_dmy_to_ymd(date)
-    if date.nil?
-      ""
-    else
-      Date.strptime(date, '%d-%m-%Y').strftime('%Y-%m-%d')
+
+  def parse_start_date(date)
+    result = date_dmy_to_db(date)
+    result ? result + ' 00:00:00' : false
+  end
+
+  def parse_end_date(date)
+    result = date_dmy_to_db(date)
+    result ? result + ' 23:59:59' : false
+  end
+
+  def date_dmy_to_db(date)
+    begin
+      if date.nil?
+        ""
+      else
+        Date.strptime(date, '%d-%m-%Y').strftime('%Y-%m-%d')
+      end
+    rescue
+      false
     end
   end
 end
