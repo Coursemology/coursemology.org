c74bcf91b357a611461e1cb6d168969002babb15
Minqi Chen <cnchenminqi@gmail.com>
2013-11-02 23:07:37 +0800

    Ranged date search for forum posts

--- app/controllers/forum_participation_controller.rb
+++ app/controllers/forum_participation_controller.rb
@@ -4,16 +4,21 @@ class ForumParticipationController < ApplicationController
   before_filter :load_general_course_data, only: [:manage]
 
   def manage
+    @from_date = params[:from] || '01-10-2013'
+    @to_date = params[:to] || '30-11-2013'
+    from_date_db = date_dmy_to_ymd(@from_date) + ' 00:00:00'
+    to_date_db = date_dmy_to_ymd(@to_date) + ' 23:59:59'
+
     @students_courses = @course.user_courses.student.order('lower(name)')
     category ||= Forem::Category.find(@course.id)
-    posts ||= Forem::Forum.joins(topics: :posts).where('category_id' => category.id)
-    result = posts.select('COUNT(*) as post_count, forem_posts.user_id').group('forem_posts.user_id')
+    result = Forem::Forum.
+        joins(topics: :posts).
+        where('category_id = ? AND forem_posts.created_at >= ? AND forem_posts.created_at <= ?',
+              category.id, from_date_db, to_date_db).select('COUNT(*) as post_count, forem_posts.user_id').group('forem_posts.user_id')
     @post_count = {}
     result.each {|i| @post_count[i.user_id] = i.post_count}
     @range_selection = {}
 
-    @from_date = params[:from] || '11-01-2013'
-    @to_date = params[:to] || '31-01-2013'
     sort_key = ''
 
     if sort_column == 'Name'
@@ -36,5 +41,14 @@ class ForumParticipationController < ApplicationController
 
   def student_posts
 
+  end
+
+  private
+  def date_dmy_to_ymd(date)
+    if date.nil?
+      ""
+    else
+      Date.strptime(date, '%d-%m-%Y').strftime('%Y-%m-%d')
+    end
   end
 end
