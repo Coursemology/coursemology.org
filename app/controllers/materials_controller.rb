d86d75dd06a3ddfcea35ca0e14c280197abe5e29
Joel Low <joel@joelsplace.sg>
2013-11-02 18:43:24 +0800

    Ensure that users can only see the workbin for missions and trainings they can access.

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -39,7 +39,7 @@ class MaterialsController < ApplicationController
     # If we are the root directory, we need to include the virtual entries for
     # this course
     if @folder.parent_folder == nil then
-      @virtual_folders = @course.workbin_virtual_entries(current_ability)
+      @virtual_folders = @course.workbin_virtual_entries(current_ability, curr_user_course)
     else
       @virtual_folders = []
     end
@@ -58,7 +58,7 @@ class MaterialsController < ApplicationController
 
   def index_virtual
     # Find the virtual folder matching the specified ID
-    @folder = (@course.workbin_virtual_entries(current_ability).select {
+    @folder = (@course.workbin_virtual_entries(current_ability, curr_user_course).select {
         |folder| folder.id == params[:virtual] })
     raise ActiveRecord::RecordNotFound if @folder.length == 0
     @folder = @folder[0]
@@ -228,7 +228,7 @@ private
       build_subtree(subfolder, include_files)
     }
     if (folder.parent_folder == nil) and not (folder.is_virtual) then
-      folder_metadata['subfolders'] += @course.workbin_virtual_entries(current_ability).map { |subfolder|
+      folder_metadata['subfolders'] += @course.workbin_virtual_entries(current_ability, curr_user_course).map { |subfolder|
         build_subtree(subfolder, include_files)
       }
     end
