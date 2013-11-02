fb78434eb5c7a48d76a17386b419e0a7ca8213a3
Minqi Chen <cnchenminqi@gmail.com>
2013-11-02 15:49:49 +0800

    Fix link to most recent post in topics view

--- app/decorator/helpers/topics_helper_decorator.rb
+++ app/decorator/helpers/topics_helper_decorator.rb
@@ -1,4 +1,10 @@
 Forem::TopicsHelper.class_eval do
+  def link_to_latest_post(topic)
+    post = relevant_posts(topic).last
+    text = "#{time_ago_in_words(post.created_at)} #{t("ago_by")} #{post.user}"
+    link_to text, main_app.course_forum_topic_path(@course, post.topic.forum, post.topic, :anchor => "post-#{post.id}", :page => topic.last_page)
+  end
+
   def new_since_last_view_text(topic)
     if forem_user
       topic_view = topic.view_for(forem_user)
