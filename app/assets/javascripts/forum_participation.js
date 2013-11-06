6ea6a80dd4f5814ee8048dea38585f9b7beb1c57
Minqi Chen <cnchenminqi@gmail.com>
2013-11-07 03:08:52 +0800

    cosmetics changes

--- app/assets/javascripts/forum_participation.js
+++ app/assets/javascripts/forum_participation.js
@@ -1,10 +1,18 @@
 $(function(){
+  function append_query(href, attr) {
+      if (href.indexOf('?') < 0) {
+          return href + '?' + attr;
+      } else {
+          return href + '&' + attr
+      }
+
+  }
   $('.post-count-link').click(
       function() {
           var that = this;
           if (! $(that).data('loaded')) {
               $.ajax({
-                  url: $(that).attr('href') + '&raw=1',
+                  url: append_query($(that).attr('href'), 'raw=1'),
                   cache: false,
                   success: function(html){
                       $(that).closest('tr').next().find('.post-details').html(html);
