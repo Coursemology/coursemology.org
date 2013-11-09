5471c96f53e46868f5421cc122206c0a5095dabc
Minqi Chen <cnchenminqi@gmail.com>
2013-11-09 22:20:34 +0800

    Bump migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131109085922) do
+ActiveRecord::Schema.define(:version => 20131109155007) do
 
   create_table "achievements", :force => true do |t|
     t.string   "icon_url"
