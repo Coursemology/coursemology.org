67ff580e25ff2938f03b07d01010ca10dd9d9855
Joel Low <joel@joelsplace.sg>
2013-11-09 14:56:50 +0800

    Show a closed label if the folder is not open.

--- app/models/material_folder.rb
+++ app/models/material_folder.rb
@@ -38,6 +38,9 @@ class MaterialFolder < ActiveRecord::Base
       def can_student_upload?
         false
       end
+      def is_open?
+        true
+      end
       def files
         @files
       end
@@ -105,6 +108,11 @@ class MaterialFolder < ActiveRecord::Base
       material.description = descriptions[key]
       material.save
     end
+  end
+
+  def is_open?
+    (open_at == nil || open_at <= DateTime.new) &&
+    (close_at == nil || close_at >= DateTime.new)
   end
 
   def new_subfolder(name, description = nil)
