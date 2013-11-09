9d2fb4aab51c1337068f5370ddc53f5691d22c5c
Joel Low <joel@joelsplace.sg>
2013-11-09 15:17:30 +0800

    Implement editing the folder open/close time, and whether students can upload to the folder.

--- app/models/material_folder.rb
+++ app/models/material_folder.rb
@@ -7,7 +7,7 @@ class MaterialFolder < ActiveRecord::Base
   has_many :subfolders, dependent: :destroy, class_name: "MaterialFolder", foreign_key: "parent_folder_id"
   has_many :files, dependent: :destroy, class_name: "Material", foreign_key: "folder_id"
 
-  attr_accessible :parent_folder, :course, :course_id, :name, :description
+  attr_accessible :parent_folder, :course, :course_id, :name, :description, :can_student_upload, :open_at, :close_at
 
   # Creates a virtual item of this class that is backed by some other data store.
   def self.create_virtual(id, parent_id)
