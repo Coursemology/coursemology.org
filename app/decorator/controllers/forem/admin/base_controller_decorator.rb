c923658d78a89b5112f4f237ddee60e08ce96865
Minqi Chen <cnchenminqi@gmail.com>
2013-11-10 18:00:31 +0800

    Hack patch for missing course id

--- app/decorator/controllers/forem/admin/base_controller_decorator.rb
+++ app/decorator/controllers/forem/admin/base_controller_decorator.rb
@@ -1,5 +1,9 @@
 Forem::Admin::BaseController.class_eval do
   def authenticate_forem_admin
+  	if @course.blank?
+      @course = Course.find(params[:course_id])
+    end
+    
     if !(can? :manage, Forem)
       flash.alert = t("forem.errors.access_denied")
       redirect_to main_app.course_forums_path(@course)
