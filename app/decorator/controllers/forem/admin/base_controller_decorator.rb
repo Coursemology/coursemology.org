b67e57075401adc1ea4215c086c75af5fa626fcf
Minqi Chen <cnchenminqi@gmail.com>
2013-11-10 18:39:13 +0800

    Monkey patch for forum management

--- app/decorator/controllers/forem/admin/base_controller_decorator.rb
+++ app/decorator/controllers/forem/admin/base_controller_decorator.rb
@@ -1,7 +1,13 @@
 Forem::Admin::BaseController.class_eval do
   def authenticate_forem_admin
-  	if @course.blank?
-      @course = Course.find(params[:course_id])
+    if @course.blank?
+      if params[:course_id]
+        @course = Course.find(params[:course_id])
+      elsif params[:id]
+        @course = Course.find(Forem::Forum.find(params[:id]).category.id)
+      else
+        @course = Course.find(params[:forum][:category_id])
+      end
     end
     
     if !(can? :manage, Forem)
