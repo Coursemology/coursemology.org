666028c819e465a9ffb9238a72fcbb318e8e4f2d
Jerome Cheng <jcheng31@gmail.com>
2013-11-14 05:40:55 +0000

    Add missing semicolon.

--- app/assets/javascripts/lesson_plan.js
+++ app/assets/javascripts/lesson_plan.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
         $(document.getElementsByClassName(data.entryType)).slideUp();
       }
     }
-  })
+  });
   
   $('#lesson-plan-hide-all').click(function() {
     $('.lesson-plan-body').slideUp();
