7f310cddef5b21eaa2ee382ff2b9d9713486c589
Joel Low <joel@joelsplace.sg>
2013-11-14 11:13:50 +0800

    Display filters for the predefined types as well.

--- app/helpers/lesson_plan_helper.rb
+++ app/helpers/lesson_plan_helper.rb
@@ -1,7 +1,9 @@
 module LessonPlanHelper
   # Returns a hash of the human-readable type, to the CSS class of the type of entry
   def get_lesson_plan_entry_types
-    result = {}
+    result = LessonPlanEntry::ENTRY_TYPES.map { |type|
+      type[0]
+    }
     virtual_entry_types = @course.lesson_plan_virtual_entries.reduce([]) { |rest, e|
       if rest.include?(e.entry_real_type) then
         rest
@@ -9,6 +11,8 @@ module LessonPlanHelper
         rest << e.entry_real_type
       end
     }
+
+    result + virtual_entry_types
   end
 
   def get_lesson_plan_entry_css_class(type)
