ac50ea15bd70cacab967ee7a3ad7b3e52701061b
Joel Low <joel@joelsplace.sg>
2013-12-07 17:13:48 +0800

    Implementing setting topic types.

--- app/controllers/forums/topics_controller.rb
+++ app/controllers/forums/topics_controller.rb
@@ -5,7 +5,7 @@ class Forums::TopicsController < ApplicationController
   before_filter :load_forum
   load_and_authorize_resource :forum
   before_filter :load_topic
-  load_and_authorize_resource :topic
+  load_and_authorize_resource :topic, class: ForumTopic
 
   def show
     respond_to do |format|
@@ -43,6 +43,21 @@ class Forums::TopicsController < ApplicationController
     end
   end
 
+  def set_type
+    @topic.topic_type = params[:type]
+    respond_to do |format|
+      if @topic.save then
+        format.html { redirect_to course_forum_topic_path(@course, @forum, @topic),
+                                  notice: 'The topic type was successfully updated.' }
+        format.json { { status: 'OK' } }
+      else
+        format.html { redirect_to course_forum_topic_path(@course, @forum, @topic),
+                                  flash: { error: 'The topic type could not be updated.' } }
+        format.json { { status: 'Error' } }
+      end
+    end
+  end
+
   def destroy
     @topic.destroy
 
@@ -58,7 +73,7 @@ private
   end
 
   def load_topic
-    @topic = ForumTopic.find_using_slug(params[:id])
+    @topic = ForumTopic.find_using_slug(params[:id] || params[:topic_id])
     if %w(new create).include?(params[:action])
       @topic = ForumTopic.new
       # No implicit assignment of attributes to the @post property. We need to create the post first before
