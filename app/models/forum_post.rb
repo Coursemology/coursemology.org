e9f6659b2f27871a52e240b0d2cb86bd671488d0
Joel Low <joel@joelsplace.sg>
2013-12-07 14:32:48 +0800

    When deleting a post, nullify all child post's links.

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -2,7 +2,7 @@ class ForumPost < ActiveRecord::Base
   belongs_to :topic, class_name: 'ForumTopic', foreign_key: 'topic_id'
   belongs_to :author, class_name: 'UserCourse', foreign_key: 'author_id'
   belongs_to :parent, class_name: 'ForumPost', foreign_key: 'parent_id'
-  has_many :children, class_name: 'ForumPost', foreign_key: 'parent_id'
+  has_many :children, class_name: 'ForumPost', foreign_key: 'parent_id', dependent: :nullify
 
   attr_accessible :title, :text, :parent_id
 
