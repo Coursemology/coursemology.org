159f7920a4f144160823edb0d683d67e5ecf6c05
Joel Low <joel@joelsplace.sg>
2013-12-07 14:47:51 +0800

    For now, when we click on reply, scroll to the reply box. When we fully implement this, the reply bow should appear below the post we are replying to.

--- app/assets/javascripts/forums/forums.js
+++ app/assets/javascripts/forums/forums.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   "use strict";
   $('#topic .btn.reply').click(function(e) {
-    $('#topic #forum_post_parent_id').val($(this).data('postId'));
+    $('#topic #forum_post_parent_id').val($(this).data('postId'))
+    $('html, body').animate({
+      scrollTop: $('.post-content').offset().top
+    }, 500);
+
     e.preventDefault();
   })
 });
