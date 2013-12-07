02c40d94053d63d643e12aeeabebf347434afdd9
Joel Low <joel@joelsplace.sg>
2013-12-07 17:21:49 +0800

    Implement setting whether a topic is hidden or locked from the topic page.

--- app/controllers/forums/topics_controller.rb
+++ app/controllers/forums/topics_controller.rb
@@ -44,6 +44,7 @@ class Forums::TopicsController < ApplicationController
   end
 
   def set_type
+    authorize! :edit, @topic
     @topic.topic_type = params[:type]
     respond_to do |format|
       if @topic.save then
@@ -57,6 +58,41 @@ class Forums::TopicsController < ApplicationController
       end
     end
   end
+
+  def set_lock
+    authorize! :edit, @topic
+    @topic.locked = params[:lock]
+
+    respond_to do |format|
+      if @topic.save then
+        format.html { redirect_to course_forum_topic_path(@course, @forum, @topic),
+                                  notice: "The topic was was successfully #{params[:lock] ? 'locked' : 'unlocked'}." }
+        format.json { { status: 'OK' } }
+      else
+        format.html { redirect_to course_forum_topic_path(@course, @forum, @topic),
+                                  flash: { error: "The topic type could not be #{params[:lock] ? 'locked' : 'unlocked'}." } }
+        format.json { { status: 'Error' } }
+      end
+    end
+  end
+
+  def set_hide
+    authorize! :edit, @topic
+    @topic.hidden = params[:hide]
+
+    respond_to do |format|
+      if @topic.save then
+        format.html { redirect_to course_forum_topic_path(@course, @forum, @topic),
+                                  notice: "The topic was was successfully #{params[:hide] ? 'hidden' : 'shown'}." }
+        format.json { { status: 'OK' } }
+      else
+        format.html { redirect_to course_forum_topic_path(@course, @forum, @topic),
+                                  flash: { error: "The topic type could not be #{params[:hide] ? 'hidden' : 'shown'}." } }
+        format.json { { status: 'Error' } }
+      end
+    end
+  end
+
 
   def destroy
     @topic.destroy
