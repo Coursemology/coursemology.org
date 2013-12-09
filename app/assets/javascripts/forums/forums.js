81aaf1cacb2b5db2f158d65f45a0868a357bf099
Joel Low <joel@joelsplace.sg>
2013-12-09 09:49:25 +0800

    Then scroll to the reply panel when we click on Reply, in case we clicked on the topic-level reply button.

--- app/assets/javascripts/forums/forums.js
+++ app/assets/javascripts/forums/forums.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
   "use strict";
   $('.forum .btn.reply').click(function(e) {
     // Set the post we are replying to.
+    var post_id = parseInt($(this).data('postId'));
     var $quick_reply = $('.forum .quick-reply');
-    $('#forum_post_parent_id', $quick_reply).val($(this).data('postId'));
+    $('#forum_post_parent_id', $quick_reply).val(post_id);
 
     // Reattach the form beneath the post we are replying to.
+    var $post = $('.forum div#post-' + post_id);
     $quick_reply.hide();
     $quick_reply.detach();
-    $quick_reply.appendTo($(this).parents('div.contents'));
+    $quick_reply.appendTo($('div.contents', $post));
     $quick_reply.slideDown();
+
+    $('html, body').animate({
+      scrollTop: $quick_reply.offset().top
+    }, 500);
 
     e.preventDefault();
   })
