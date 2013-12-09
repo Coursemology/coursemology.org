a93ddec5aaf798f3e767ba0f203f63f3098842e6
Joel Low <joel@joelsplace.sg>
2013-12-09 08:26:55 +0800

    Fix inline posting.

--- app/assets/javascripts/forums/forums.js
+++ app/assets/javascripts/forums/forums.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   "use strict";
-  $('#topic .btn.reply').click(function(e) {
+  $('.forum .btn.reply').click(function(e) {
     $('#topic #forum_post_parent_id').val($(this).data('postId'))
     $('html, body').animate({
       scrollTop: $('.post-content').offset().top
