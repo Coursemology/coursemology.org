f2535d8a36d506184a1f9cfdc8d38773483ce598
Joel Low <joel@joelsplace.sg>
2013-12-09 09:07:21 +0800

    When replying to a post, move the entire editor to the post so the user can craft his response in context.

--- app/assets/javascripts/forums/forums.js
+++ app/assets/javascripts/forums/forums.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   "use strict";
   $('.forum .btn.reply').click(function(e) {
-    $('#topic #forum_post_parent_id').val($(this).data('postId'))
-    $('html, body').animate({
-      scrollTop: $('.post-content').offset().top
-    }, 500);
+    // Set the post we are replying to.
+    var $quick_reply = $('.forum .quick-reply');
+    $('#forum_post_parent_id', $quick_reply).val($(this).data('postId'));
+
+    // Reattach the form beneath the post we are replying to.
+    $quick_reply.hide();
+    $quick_reply.detach();
+    $quick_reply.appendTo($(this).parents('div.contents'));
+    $quick_reply.slideDown();
 
     e.preventDefault();
   })
