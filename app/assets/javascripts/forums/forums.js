9c4be77b8632519a477ce9c9096fd7a1e5f8b244
Joel Low <joel@joelsplace.sg>
2013-12-11 09:38:36 +0800

    Automatically generate post reply titles.

--- app/assets/javascripts/forums/forums.js
+++ app/assets/javascripts/forums/forums.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
   "use strict";
+
+  /**
+   * Prepend the given post title with a RE: if it does not already have one.
+   * @param title The title to make into a reply.
+   * @returns string
+   */
+  function replize_title(title) {
+    if (/re:/i.test(title)) {
+      return title;
+    } else {
+      return 'RE: ' + title;
+    }
+  }
+
   $('.forum .btn.reply').click(function(e) {
     // Set the post we are replying to.
     var post_id = parseInt($(this).data('postId'));
+    var post_title = replize_title($(this).data('postTitle'));
     var $quick_reply = $('.forum .quick-reply');
+    $('#forum_post_title', $quick_reply).val(post_title);
     $('#forum_post_parent_id', $quick_reply).val(post_id);
 
     // Reattach the form beneath the post we are replying to.
--- app/helpers/forums/forums_helper.rb
+++ app/helpers/forums/forums_helper.rb
@@ -2,4 +2,15 @@ module Forums::ForumsHelper
   def topic_latest_post_path(course, forum, topic)
     course_forum_topic_path(course, forum, topic, anchor: "post-#{topic.posts.last.id}")
   end
+
+  # Prepend the given post title with a RE: if it does not already have one.
+  # @param title The title to make into a reply.
+  # @returns string
+  def replize_title(title)
+    if /^re:/ =~ title
+      title
+    else
+      'RE: ' + title
+    end
+  end
 end
