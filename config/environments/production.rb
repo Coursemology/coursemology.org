7cbc9be37e55322c45168f241fb85cda0f01d219
Raymond <raymondtangsc@gmail.com>
2013-12-17 16:31:41 +0800

    paperclip preserver old file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,12 @@ JfdiAcademy::Application.configure do
   }
 
   config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-          :bucket => ENV['AWS_BUCKET'],
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      preserve_files: true,
+      storage:        :s3,
+      s3_credentials: {
+          bucket:             ENV['AWS_BUCKET'],
+          access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
       }
   }
 end
