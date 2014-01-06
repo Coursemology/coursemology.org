bbc3849b71fee583e68dee6095a44f9acad64a59
Joel Low <joel@joelsplace.sg>
2014-01-06 19:58:25 +0800

    Added indices to reference columns.

--- db/migrate/20140106165617_assessments_redesign.rb
+++ db/migrate/20140106165617_assessments_redesign.rb
@@ -1,8 +1,8 @@
 class AssessmentsRedesign < ActiveRecord::Migration
   def change
     create_table :assessment_assessments do |t|
-      t.integer :course_id
-      t.integer :creator_id
+      t.references :course
+      t.references :creator
       t.string :title
       t.text :description
 
@@ -18,7 +18,7 @@ class AssessmentsRedesign < ActiveRecord::Migration
     end
 
     create_table :assessment_tags do |t|
-      t.integer :assessment_id
+      t.references :assessment
     end
 
     create_table :assessment_assessment_requirements do |t|
@@ -26,13 +26,13 @@ class AssessmentsRedesign < ActiveRecord::Migration
     end
 
     create_table :assessment_questions do |t|
-      t.integer :assessment_id
+      t.references :assessment, index: true
       t.integer :max_grade
       t.string :type
     end
 
     create_table :assessment_coding_questions do |t|
-      t.integer :creator_id
+      t.references :creator
       t.string :title # was step_name
       t.text :description
       t.text :staff_comment
@@ -42,7 +42,7 @@ class AssessmentsRedesign < ActiveRecord::Migration
     end
 
     create_table :assessment_mcq_questions do |t|
-      t.integer :creator_id
+      t.references :creator
       t.text :description
       t.boolean :must_select_all, :default => false # was select_all
       t.integer :max_grade
@@ -51,8 +51,8 @@ class AssessmentsRedesign < ActiveRecord::Migration
     end
 
     create_table :assessment_mcq_options do |t| # was mcq_answers
-      t.integer :creator_id
-      t.integer :question_id # was mcq_id
+      t.references :creator
+      t.references :question, index: true # was mcq_id
       t.text :text
       t.text :explanation
       t.boolean :correct # was is_correct
@@ -61,7 +61,7 @@ class AssessmentsRedesign < ActiveRecord::Migration
     end
 
     create_table :assessment_text_questions do |t|
-      t.integer :creator_id
+      t.references :creator
       t.text :description
       t.integer :max_grade
 
@@ -72,8 +72,8 @@ class AssessmentsRedesign < ActiveRecord::Migration
       # Instead of finalising a submission, we finalise question by question.
       # This allows trainings to be finalised step-by-step.
       # To finalise the entire mission submission, for example, set all questions as finalised.
-      t.integer :assessment_id # was mission_id/training_id
-      t.integer :std_course_id
+      t.references :assessment, index: true # was mission_id/training_id
+      t.referneces :std_course, index: true
       t.string :status
       t.float :multiplier
       t.datetime :opened_at # was open_at
@@ -83,50 +83,52 @@ class AssessmentsRedesign < ActiveRecord::Migration
       t.datetime :deleted_at
 
       # Number of attempts is computed by the number of gradings.
+
+      t.index [:status]
     end
 
     create_table :assessment_question_submissions do |t|
-      t.integer :submission_id
-      t.integer :question_id
+      t.references :submission, index: true
+      t.references :question, index: true
       t.boolean :finalised
 
       t.timestamps
     end
 
     create_table :assessment_coding_submissions do |t|
-      t.integer :question_submission_id
+      t.references :question_submission, index: true
       t.text :code
 
       t.timestamps
     end
 
     create_table :assessment_mcq_submissions do |t|
-      t.integer :question_submission_id
-      t.integer :option_id
+      t.references :question_submission, index: true
+      t.references :option
 
       t.timestamps
     end
 
     create_table :assessment_text_submissions do |t|
-      t.integer :question_submission_id
+      t.references :question_submission, index: true
       t.text :text
 
       t.timestamps
     end
 
     create_table :assessment_gradings do |t|
-      t.integer :question_submission_id
-      t.integer :grader
+      t.references :question_submission, index: true
+      t.references :grader
+      t.references :grader_course # curr_user_course for the grader
       t.integer :grade
       t.text :comment
-      t.integer :exp_transaction_id
-      t.integer :grader_course_id # curr_user_course for the grader
+      t.references :exp_transaction
 
       t.timestamps
     end
 
     create_table :assessment_trainings do |t|
-      t.integer :assessment_id
+      t.references :assessment, index: true
       t.integer :pos
 
       t.integer :bonus_exp
@@ -134,7 +136,7 @@ class AssessmentsRedesign < ActiveRecord::Migration
     end
 
     create_table :assessment_missions do |t|
-      t.integer :assessment_id
+      t.references :assessment, index: true
       t.integer :pos
 
       t.integer :dependent_id
