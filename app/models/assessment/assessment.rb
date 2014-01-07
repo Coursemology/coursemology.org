c766b38cc507910c9afa86add14931ea5d4d44a9
Joel Low <joel@joelsplace.sg>
2014-01-07 13:22:01 +0800

    Implemented the ability to display the requirements to do this assessment.

--- app/models/assessment/assessment.rb
+++ app/models/assessment/assessment.rb
@@ -2,7 +2,10 @@ class Assessment::Assessment < ActiveRecord::Base
   acts_as_superclass
 
   has_and_belongs_to_many :tags, class_name: Tag, join_table: :assessment_assessments_tags
-  has_many :requirements, class_name: Requirement
+  has_many :assessment_requirements, class_name: AssessmentsRequirement, dependent: :destroy
+  has_many :requirements, class_name: Requirement, through: :assessment_requirements
   belongs_to :course, class_name: 'Course'
   belongs_to :creator, class_name: 'User'
+
+  alias :as_requirements :requirements
 end
--- app/models/assessment/assessments_requirement.rb
+++ app/models/assessment/assessments_requirement.rb
@@ -1,4 +1,4 @@
 class Assessment::AssessmentsRequirement < ActiveRecord::Base
   has_one :assessment, class_name: 'Assessment::Assessment'
-  belongs_to :requirement, class_name: 'Requirement'
+  has_many :requirement, class_name: 'Requirement', foreign_key: :obj_id, conditions: { requirements: { obj_type: Assessment::AssessmentsRequirement } }
 end
