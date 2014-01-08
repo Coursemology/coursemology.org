72c0c9aafcfa7a592574319ec24780029e002899
Joel Low <joel@joelsplace.sg>
2014-01-08 10:04:58 +0800

    Implement creation and display of coding questions in missions.

--- app/models/assessment/coding_question.rb
+++ app/models/assessment/coding_question.rb
@@ -3,4 +3,6 @@ class Assessment::CodingQuestion < ActiveRecord::Base
 
   has_one :depends_on, foreign_key: 'depends_on', class_name: 'Assessment::CodingQuestion'
   has_one :comment_topic, as: :topic
+
+  attr_accessible :title, :description, :max_grade, :time_limit, :memory_limit, :data, :depends_on
 end
