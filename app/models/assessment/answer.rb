d8b72e1e0110cca42de593ff26900e2e72f40825
Joel Low <joel@joelsplace.sg>
2014-01-08 20:02:37 +0800

    Implement editing a submission.

--- app/models/assessment/answer.rb
+++ app/models/assessment/answer.rb
@@ -1,6 +1,8 @@
 class Assessment::Answer < ActiveRecord::Base
   acts_as_superclass as: :as_assessment_answer
 
+  has_one :comment_topic, as: :topic
+
   belongs_to :submission
   belongs_to :question
 end
