e84ed89f016430a13afbd9ba75266b17ff410dd3
Joel Low <joel@joelsplace.sg>
2014-01-08 12:08:50 +0800

    Fix saving of dependencies and the language for coding questions.

--- app/models/assessment/coding_question.rb
+++ app/models/assessment/coding_question.rb
@@ -1,8 +1,8 @@
 class Assessment::CodingQuestion < ActiveRecord::Base
   is_a :question, as: 'as_assessment_question', class_name: 'Assessment::Question'
 
-  has_one :depends_on, foreign_key: 'depends_on', class_name: 'Assessment::CodingQuestion'
+  has_one :depends_on, class_name: 'Assessment::CodingQuestion'
   has_one :comment_topic, as: :topic
 
-  attr_accessible :title, :description, :max_grade, :time_limit, :memory_limit, :data, :depends_on
+  attr_accessible :title, :description, :max_grade, :language, :time_limit, :memory_limit, :test_limit, :data, :depends_on_id
 end
