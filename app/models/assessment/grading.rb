90f357e89e6eea2c086bde1d7edf48972faa37e4
Joel Low <joel@joelsplace.sg>
2014-01-09 18:47:17 +0800

    Implement validators for grading.

--- app/models/assessment/grading.rb
+++ app/models/assessment/grading.rb
@@ -1,6 +1,24 @@
 class Assessment::Grading < ActiveRecord::Base
-  belongs_to :question_submission
+  belongs_to :answer
   belongs_to :grader, class_name: 'User'
   belongs_to :grader_course, class_name: 'UserCourse'
   belongs_to :exp_transaction, class_name: 'ExpTransaction'
+
+  attr_accessible :grade
+
+  validate :grade_less_than_question_maximum
+  validate :exp_less_than_assignment_maximum
+
+private
+  def grade_less_than_question_maximum
+    if grade && answer.question.max_grade < grade then
+      errors.add(:grade, 'cannot be greater than question maximum')
+    end
+  end
+
+  def exp_less_than_assignment_maximum
+    if exp_transaction && answer.question.assessment.exp < exp_transaction.exp then
+      errors.add(:exp, 'cannot be greater than assignment maximum')
+    end
+  end
 end
