cec40a35aae2e0b452434e7a05ef809fdfbcf664
Raymond <raymondtangsc@gmail.com>
2014-01-13 07:00:00 +0800

    move my courses to home controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -187,14 +187,4 @@ class CoursesController < ApplicationController
     @pending_gradings = @course.get_pending_gradings(curr_user_course)
   end
 
-  def my_courses
-    @courses = current_user.courses
-
-    @courses_std = current_user.user_courses.student.map { |uc| uc.course }
-    @courses_staff = current_user.user_courses.staff.map { |uc| uc.course }
-    @courses_shared = current_user.user_courses.shared.map { |uc| uc.course }
-    if @courses.count == 0
-      @all_courses = Course.where(is_publish: true)
-    end
-  end
 end
