8fe799aed8b1a336a79ffd50a58e5c1d23d7331f
Joel Low <joel@joelsplace.sg>
2014-01-19 14:14:47 +0800

    Make sure we have a valid session before trying to log the user out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
 
   def destroy
-    current_user.is_logged_in = false
+    current_user.is_logged_in = false if current_user
     super
   end
 end
