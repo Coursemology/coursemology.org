1759195794b2d00f15b90cde164944b3ed6b0186
Raymond <raymondtangsc@gmail.com>
2014-06-05 23:31:50 +0800

    fix for user able to change their role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,16 +3,17 @@ class Ability
 
   def initialize(user)
     if user
+      can :manage, User, id: user.id
+      can :manage, RoleRequest, user_id: user.id
+      cannot :update_role, User
+
       if user.is_admin?
         can :manage, :all
         can :masquerade, :user
-        can :update_role, :all
+        can :update_role, User
       elsif user.is_lecturer?
         can :manage, Course, creator_id: user.id
       end
-
-      can :manage, User, id: user.id
-      can :manage, RoleRequest, user_id: user.id
     end
     can :read, :all
   end
