c8601af0db125dc267824c6cb5d4ac361fecad34
Raymond <raymondtangsc@gmail.com>
2014-06-12 19:13:48 +0800

    improve date validator to use .send

--- app/models/modules/date_validator.rb
+++ app/models/modules/date_validator.rb
@@ -5,7 +5,7 @@ class DateValidator < ActiveModel::Validator
   end
   
   def validate(record)
-    if record[@fields[0]] > record[@fields[1]]
+    if record.send(@fields[0]) > record.send(@fields[1])
       record.errors[:base] << "End time should be after start time."
     end
   end
