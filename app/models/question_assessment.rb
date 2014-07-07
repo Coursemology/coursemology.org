e8b11b4f8da97eb6a85c0f330f8a4f06bd596be0
Raymond <raymondtangsc@gmail.com>
2014-07-08 05:18:44 +0800

    add class name for question  in question assessment relationship model

--- app/models/question_assessment.rb
+++ app/models/question_assessment.rb
@@ -2,5 +2,5 @@ class QuestionAssessment < ActiveRecord::Base
 
   belongs_to :assessment
   #TOFIX, if we put depend: :destroy here, what will happen if question is pointed to multiple assessments
-  belongs_to :question
+  belongs_to :question, class_name: "Assessment::Question"
 end
