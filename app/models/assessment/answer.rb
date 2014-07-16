6cb4e2ea78f1eb19b396e0822d6194847f26d522
Raymond <raymondtangsc@gmail.com>
2014-07-16 16:38:44 +0800

    add relation method unique_attempts in assessment answer model, this will get all unique answer of student for mcq question

--- app/models/assessment/answer.rb
+++ app/models/assessment/answer.rb
@@ -2,15 +2,18 @@ class Assessment::Answer < ActiveRecord::Base
   acts_as_paranoid
 
   scope :coding,
-        joins("INNER JOIN assessment_questions on assessment_answers.question_id =
+        -> { joins("INNER JOIN assessment_questions on assessment_answers.question_id =
             assessment_questions.id and assessment_questions.as_question_type = 'Assessment::CodingQuestion'")
-        .readonly(false)
+        .readonly(false) }
+
+  scope :finalised,  -> { where(finalised: true) }
 
   belongs_to  :question, class_name: Assessment::Question
   belongs_to  :std_course, class_name: "UserCourse"
   belongs_to  :submission, class_name: Assessment::Submission
   has_many  :annotations, as: :annotable, dependent: :destroy
-  has_one   :answer_grading, class_name: Assessment::AnswerGrading
+  has_one   :answer_grading, class_name: Assessment::AnswerGrading, dependent: :destroy
+  has_many  :answer_options, class_name: Assessment::AnswerOption
 
   has_one :comment_topic, as: :topic
 
@@ -25,6 +28,16 @@ class Assessment::Answer < ActiveRecord::Base
   # def qn_id
   #   self.question_id
   # end
+
+  def self.unique_attempts(correct = nil)
+    options = Assessment::AnswerOption.select(:option_id).where("assessment_answer_options.answer_id IN (?)", self.all).
+        joins("LEFT JOIN assessment_mcq_options ON assessment_mcq_options.id = assessment_answer_options.option_id").uniq
+    if correct.nil?
+      options
+    else
+      options.where("assessment_mcq_options.correct = ?", correct)
+    end
+  end
 
   def result_hash
     self.answer ? JSON.parse(self.result) : {}
