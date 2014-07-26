c8496567691682c6af9c4edc804074f14c279233
Raymond <raymondtangsc@gmail.com>
2014-07-26 15:15:44 +0800

    change default date format in date picker

--- app/inputs/datetime_picker_input.rb
+++ app/inputs/datetime_picker_input.rb
@@ -1,11 +1,11 @@
 class DatetimePickerInput < SimpleForm::Inputs::StringInput
   def input
     value = object.send(attribute_name) if object.respond_to? attribute_name
-    display_pattern = I18n.t('datepicker.dformat', :default => '%d/%m/%Y') + ' ' + I18n.t('timepicker.dformat', :default => '%R')
+    display_pattern = I18n.t('datepicker.dformat', :default => '%d-%m-%Y') + ' ' + I18n.t('timepicker.dformat', :default => '%R')
     input_html_options[:value] ||= I18n.localize(value, :format => display_pattern) if value.present?
 
     input_html_options[:type] = 'text'
-    picker_pettern = I18n.t('datepicker.pformat', :default => 'dd/MM/yyyy') + ' ' + I18n.t('timepicker.pformat', :default => 'hh:mm')
+    picker_pettern = I18n.t('datepicker.pformat', :default => 'dd-MM-yyyy') + ' ' + I18n.t('timepicker.pformat', :default => 'hh:mm')
     input_html_options[:data] ||= {}
     input_html_options[:data].merge!({ format: picker_pettern, language: I18n.locale.to_s,
                                        date_weekstart: I18n.t('datepicker.weekstart', :default => 0) })
