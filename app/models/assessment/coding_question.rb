989771076750afa5983b75ec163df52473bfe01c
Raymond <raymondtangsc@gmail.com>
2014-07-26 15:29:26 +0800

    fix accessible attributes for question sub models

--- app/models/assessment/coding_question.rb
+++ app/models/assessment/coding_question.rb
@@ -2,56 +2,36 @@ class Assessment::CodingQuestion < ActiveRecord::Base
   acts_as_paranoid
   is_a :question, as: :as_question, class_name: "Assessment::Question"
 
+  attr_accessible :auto_graded
   attr_accessible :language_id, :dependent_id
-  attr_accessible :memory_limit, :time_limit, :test_limit, :data, :auto_graded
+  attr_accessible :title, :description, :max_grade, :attempt_limit, :staff_comments
+  attr_accessible :memory_limit, :time_limit, :tests,
+                  :template, :pre_include, :append_code
 
-  belongs_to  :dependent_on, class_name: "Assessment::Question", foreign_key: "dependent_id"
   belongs_to  :language, class_name: "ProgrammingLanguage"
 
-  before_create :set_default_data
-  after_update :update_test_limit, if: :test_limit_changed?
-  after_save :refresh_asm_autograding, :if => :data_changed?
+  after_save :refresh_asm_autograding, :if => :tests_changed?
+
+  def coding_answers
+    Assessment::CodingAnswer.
+        joins("INNER JOIN assessment_answers aa
+                  ON aa.as_answer_id = assessment_coding_answers.id AND aa.as_answer_type = 'Assessment::CodingAnswer'
+               INNER JOIN (SELECT aq.id FROM assessment_questions aq INNER JOIN assessment_coding_questions acq
+                  ON aq.as_question_id = acq.id AND aq.as_question_type = 'Assessment::CodingQuestion'
+                  WHERE acq.id = #{self.id}) aacq ON aacq.id = aa.question_id")
+  end
 
   def data_hash
-    JSON.parse(self.data)
+    JSON.parse(self.tests)
   end
-
-  #TODO
-  def set_default_data
-    unless self.data
-      self.data = '{"type":"do","prefill":""}'
-    end
-  end
-
-  def prefilled_code
-    data_hash["prefill"] || "#Prefilled code \n#Auto generated"
-  end
-
-  def included_code
-    data_hash["included"] || ""
-  end
-
-  def test_code
-    data_hash["included"] || ""
-  end
-
-  def update_test_limit
-    old_tl = changed_attributes[:test_limit] || 0
-    diff = test_limit - old_tl
-    if diff != 0
-      Thread.start {
-        std_coding_answers.each do |std_answer|
-          std_answer.test_left = [0, std_answer.test_left + diff].max
-          std_answer.save
-        end
-      }
-    end
-  end
-
 
   def refresh_asm_autograding
     if question_assessments.first
       question_assessments.first.assessment.mark_refresh_autograding
     end
+  end
+
+  def self.reflect_on_association(association)
+    super || self.parent.reflect_on_association(association)
   end
 end
