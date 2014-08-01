7445be259f35621944357547afb2ddd0c9dcb0b6
Raymond <raymondtangsc@gmail.com>
2014-08-01 14:01:40 +0800

    none user can also access courses page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Course, is_publish: true
     if user
       can :manage, User, id: user.id
       can :manage, RoleRequest, user_id: user.id
-      can :read, Course
       cannot :update_role, User
 
       if user.is_admin?
