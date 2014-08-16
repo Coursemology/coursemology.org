f570ea89652f8a4f2d9e917c3a3f0f5e5fb9a974
fonglh <fonglh@msn.com>
2014-08-16 21:17:15 +0800

    Catch APIErrors if posting to FB with Koala fails. No error is shown to the user but the modal closes. Only show FB share button if there is an FB access token in the session.
    References Coursemology/coursemology.org#100.

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -3,7 +3,14 @@ class FacebookController < ApplicationController
     get_fb_app_namespace
 
     @graph = Koala::Facebook::API.new(session[:fb_access_token])
-    @graph.put_connections("me", "#{@app_namespace}:obtain", :badge => params[:facebook_obj_id], "fb:explicitly_shared" => true)
+    begin
+      @graph.put_connections("me", "#{@app_namespace}:obtain", :badge => params[:facebook_obj_id], "fb:explicitly_shared" => true)
+    rescue Koala::Facebook::APIError => e
+      # catch and log errors posting to the fb
+      logger.error e.fb_error_message
+      # TODO: might be able to improve user experience by redirecting
+      # and asking for permission
+    end
 
     respond_to do |format|
       format.js
