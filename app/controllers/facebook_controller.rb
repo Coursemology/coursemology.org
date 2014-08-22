57ddb49399d4503f1725bdd95d1c1365ff302f0c
fonglh <fonglh@msn.com>
2014-08-22 23:04:04 +0800

    Put 'Share to Facebook' button back. Add additional logic to facebook controller to check for publish_actions permission.
    References Coursemology/coursemology.org#179.

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -3,13 +3,24 @@ class FacebookController < ApplicationController
     get_fb_app_namespace
 
     @graph = Koala::Facebook::API.new(session[:fb_access_token])
-    begin
-      @graph.put_connections("me", "#{@app_namespace}:obtain", :badge => params[:facebook_obj_id], "fb:explicitly_shared" => true)
-    rescue Koala::Facebook::APIError => e
-      # catch and log errors posting to the fb
-      logger.error e.fb_error_message
+    # check for publish_actions, permissions is of class GraphCollection
+    # which extends Array, so need to index it first to get the hash
+    permissions = @graph.get_connections("me", "permissions")
+    if permissions[0]["publish_actions"].nil? 
+      # no publish_actions
       # TODO: might be able to improve user experience by redirecting
       # and asking for permission
+      logger.error "no publish_actions permission, booo"
+      session[:fb_permissions] = 'publish_actions'
+
+      # check if user has been asked 3 times
+    else
+      begin
+        @graph.put_connections("me", "#{@app_namespace}:obtain", :badge => params[:facebook_obj_id], "fb:explicitly_shared" => true)
+      rescue Koala::Facebook::APIError => e
+        # catch and log errors posting to the fb
+        logger.error e.fb_error_message
+      end
     end
 
     respond_to do |format|
