d60178ab191a2590ac502220c8566cf0c9a8b6f2
Raymoond <raymondtangsc@gmail.com>
2014-09-05 12:00:29 +0800

    [migrations] remove and fix invalid migration files
    db:migrate run without error for empty database

--- db/migrate/20131109145751_remove_forem_admin_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveForemAdminFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :forem_admin
-  end
-
-  def down
-    add_column :users, :forem_admin, :TINYINT
-  end
-end
--- db/migrate/20131109145848_remove_forem_state_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveForemStateFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :forem_state
-  end
-
-  def down
-    add_column :users, :forem_state, :VARCHAR
-  end
-end
