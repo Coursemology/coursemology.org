5c505990768db8b0301cfe9e895851cf10c1c85f
Toh Weiqing <weiqingtoh@gmail.com>
2014-09-30 19:27:45 +0800

    fixed migration bug (MAJOR ERROR!)

--- db/migrate/20140927074451_create_assessment_dependency_table.rb
+++ db/migrate/20140927074451_create_assessment_dependency_table.rb
@@ -1,7 +1,7 @@
 class CreateAssessmentDependencyTable < ActiveRecord::Migration
   def up
     execute "CREATE TABLE assessment_dependency AS
-              SELECT as_assessment_id, dependent_id
+              SELECT id, dependent_id
               FROM assessments
               WHERE dependent_id IS NOT NULL and dependent_id <> 0"
   end
