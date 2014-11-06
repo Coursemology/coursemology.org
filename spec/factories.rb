93f9b513f1efd598733e2f29f67ac1fda15a51bf
fonglh <fonglh@msn.com>
2014-11-06 18:01:04 +0800

    Write some tests to test Mission editing functionality.
    Add Mission using factory girl.
    Add test to check that mission creation has the success message.
    Add test to check that edit form loads properly.
    
    Fix merge conflict with lesson plan factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -75,4 +75,15 @@ FactoryGirl.define do
     start_at 1.day.from_now
     end_at 3.days.from_now
   end
+
+  factory :mission, class: Assessment::Mission do
+     open_at Time.now
+     close_at 1.day.from_now
+     exp 10
+     after(:build) do |mission, evaluator|
+       mission.title = evaluator.title
+       mission.course_id = evaluator.course.try(:id)
+     end
+   end
+
 end
--- spec/features/mission_pages_spec.rb
+++ spec/features/mission_pages_spec.rb
@@ -46,9 +46,40 @@ describe "MissionPages", :type => :feature do
         it "should create a mission" do
           expect { click_button "Create Mission" }.to change(Assessment, :count).by(1)
         end
+
+        describe "new mission created" do
+          before do
+            click_button "Create Mission"
+          end
+
+          it "should have mission created text" do
+            expect(page).to have_text("The mission Mission 0 has been created")
+          end
+        end
       end
 
     end
+
+
+    describe "Mission Edit" do
+      let(:title) { 'Factory mission' }
+      let(:mission) {FactoryGirl.create(:mission, title: title, course: course)}
+
+      # already opens the individual mission view
+      it "should have factory mission" do
+          visit course_assessment_mission_path(course, mission)
+          expect(page).to have_text(title)
+      end
+
+      it "should have edit form labels" do
+        visit edit_course_assessment_mission_path(course, mission)
+        expect(page).to have_text('Title')
+        expect(page).to have_text('Description')
+      end
+    end
+
+
+
   end
 
 
