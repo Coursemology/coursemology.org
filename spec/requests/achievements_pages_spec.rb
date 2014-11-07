6d4feac282c1bebd14f8161e554c13b71e16aa1b
Toh Weiqing <weiqingtoh@gmail.com>
2014-11-07 10:53:13 +0800

    add delete tests for achievements. (edit tests do not work cause of absence of javascript)

--- spec/requests/achievements_pages_spec.rb
+++ spec/requests/achievements_pages_spec.rb
@@ -51,5 +51,74 @@ RSpec.describe "Achievements", :type => :request do
     end
   end
 
+  describe "edit" do
+    let!(:achievement) { FactoryGirl.create(:achievement, course: course) }
+    before { visit course_achievements_path(course) }
+    end
+
+    describe "page display" do
+      it "shows the edit button" do
+        expect(page).to have_link('', href: edit_course_achievement_path(course, achievement))
+      end
+
+      # describe "edit achievements page" do
+      #   before { visit edit_course_achievement_path(course, achievement) }
+      #
+      #   it 'renders the view correctly' do
+      #     expect(page).to have_field('Title')
+      #     expect(page).to have_field('Description')
+      #   end
+      #
+      #
+      #   context 'after changes saved' do
+      #     let(:new_title) { 'Edited Title' }
+      #     let(:new_description) { 'Edited Description' }
+      #     before do
+      #       fill_in 'Title', with: new_title
+      #       fill_in 'Description', with: new_description
+      #       click_button 'Update'
+      #     end
+      #
+      #     it 'should redirect back to index' do
+      #       expect(current_path).to eq course_achievement_path(course)
+      #       save_and_open_page
+      #     end
+      #
+      #     it 'should show the successful notice' do
+      #       expect(page).to have_content("The achievement '#{new_title}' has been updated.")
+      #     end
+      #
+      #     it 'should show changed attributes' do
+      #       achievement.reload.title.should eq new_title
+      #       achievement.reload.description.should eq new_description
+      #     end
+      #   end
+      # end
+    end
+  end
+
+  describe "delete" do
+    let!(:achievement) { FactoryGirl.save(:achievement, course: course) }
+    before { visit course_achievements_path(course) }
+
+    describe "page display" do
+      it "shows the delete button" do
+        expect(page).to have_link 'Delete Achievement'
+      end
+    end
+
+    describe "action" do
+
+      it 'should delete the achievement' do
+        expect { click_link 'Delete Achievement' }.to change(Achievement, :count).by(-1)
+      end
+    end
+
+    it 'should redirect back to index' do
+      expect(current_path).to eq course_achievements_path(course)
+    end
+
+  end
+
 
 end
