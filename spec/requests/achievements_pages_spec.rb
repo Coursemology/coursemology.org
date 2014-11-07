2662ac9aaf521963e00395eb899373dcf316d422
Toh Weiqing <weiqingtoh@gmail.com>
2014-11-08 00:00:33 +0800

    corrected pending tests. should pass travis CI as well

--- spec/requests/achievements_pages_spec.rb
+++ spec/requests/achievements_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Achievements", :type => :request do
+RSpec.describe 'Achievements', :type => :request do
   let(:admin) { FactoryGirl.create(:admin) }
   let(:course) { FactoryGirl.create(:course) }
   before do
@@ -8,43 +8,43 @@ RSpec.describe "Achievements", :type => :request do
     create_course course
   end
 
-  describe "create" do
+  describe 'create' do
     before do
       visit course_achievements_path(course)
     end
 
-    it "shows the new button" do
+    it 'shows the new button' do
       expect(page).to have_link('New', href: new_course_achievement_path(course))
     end
 
-    describe "action" do
+    describe 'action' do
       before do
         click_link 'New'
       end
 
-      it "displays the content" do
-        expect(page).to have_field("Title")
-        expect(page).to have_field("Description")
+      it 'displays the content' do
+        expect(page).to have_field('Title')
+        expect(page).to have_field('Description')
       end
 
-      describe "add valid information" do
+      describe 'add valid information' do
         let(:achievement) { FactoryGirl.build(:achievement) }
         before do
           fill_in 'Title', with: achievement.title
           fill_in 'Description', with: achievement.description
         end
 
-        pending "to mock the facebook graph connect"
-        it "create an achievement" do
+        it 'create an achievement' do
+          pending 'to mock the facebook graph connect'
           expect { click_button 'Create Achievement' }.to change(Achievement, :count).by(1)
         end
 
-        describe "after achievement is created" do
+        describe 'after achievement is created' do
           before do
             click_button 'Create'
           end
 
-          it "displays that the achievement is created" do
+          it 'displays that the achievement is created' do
             expect(page).to have_content(achievement.title)
           end
         end
@@ -52,17 +52,17 @@ RSpec.describe "Achievements", :type => :request do
     end
   end
 
-  describe "edit" do
+  describe 'edit' do
     let!(:achievement) { FactoryGirl.create(:achievement, course: course) }
     before { visit course_achievements_path(course) }
 
-    describe "page" do
-      it "shows the edit button" do
+    describe 'page' do
+      it 'shows the edit button' do
         expect(page).to have_link('', href: edit_course_achievement_path(course, achievement))
       end
 
 
-      describe "when editing achievements" do
+      describe 'when editing achievements' do
         before { visit edit_course_achievement_path(course, achievement) }
 
         it 'renders the view correctly' do
@@ -70,14 +70,13 @@ RSpec.describe "Achievements", :type => :request do
           expect(page).to have_field('Description')
         end
 
-
         context 'with changes saved' do
           let(:new_title) { 'Edited Title' }
           let(:new_description) { 'Edited Description' }
           before do
             fill_in 'Title', with: new_title
             fill_in 'Description', with: new_description
-            pending "to be implemented after javascript is enabled"
+            pending 'to be implemented after javascript is enabled'
             click_button 'Update'
           end
 
@@ -98,19 +97,21 @@ RSpec.describe "Achievements", :type => :request do
     end
   end
 
-  describe "delete" do
+  describe 'delete' do
     let!(:achievement) { FactoryGirl.create(:achievement, course: course) }
-    before { visit course_achievements_path(course) }
+    before do
+      visit course_achievements_path(course)
+    end
 
-    describe "page" do
+    describe 'page' do
       it 'shows the delete button' do
         expect(page).to have_link 'delete_achievement'
       end
     end
 
-    pending "to mock the facebook graph connect"
-    describe "action" do
+    describe 'action' do
       it 'deletes the achievement' do
+        pending 'to mock the facebook graph connect'
         expect { click_link 'delete_achievement' }.to change(Achievement, :count).by(-1)
       end
     end
@@ -118,8 +119,6 @@ RSpec.describe "Achievements", :type => :request do
     it 'redirects back to index' do
       expect(current_path).to eq course_achievements_path(course)
     end
-
   end
-
 
 end
