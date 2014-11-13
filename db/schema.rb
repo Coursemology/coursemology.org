8715dab7d2af76ac5a0fdbc0b67e71116224bbb9
Brian Koh <kxmbrian@gmail.com>
2014-11-13 15:03:30 +0800

    Add scribing answer Coursemology/coursemology.org#277

--- db/schema.rb
+++ db/schema.rb
@@ -271,14 +271,20 @@ ActiveRecord::Schema.define(:version => 20141107064604) do
   add_index "assessment_questions", ["as_question_id", "as_question_type"], :name => "index_on_as_question", :unique => true
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Add scribing answer Coursemology/coursemology.org#277
   create_table "assessment_scribing_answers", :force => true do |t|
     t.datetime "deleted_at"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
+<<<<<<< HEAD
 =======
 >>>>>>> Copy general question as template for scribing qn
+=======
+>>>>>>> Add scribing answer Coursemology/coursemology.org#277
   create_table "assessment_scribing_questions", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
