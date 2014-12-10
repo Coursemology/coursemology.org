9ce1a11aa40dc20b33a6ef3079c9e23d3fa4b04d
flh <flh@server.com>
2014-12-10 13:58:18 +0800

    Convert PDF files to PNG
    When PDF files are uploaded, run pdftoppm to convert each page to a PNG
    file. Delete PDF file afterwards.
    
    Possible security implications when putting the command together. Also, files
    are written directly to the Rails folder.

--- app/controllers/assessment/scribing_questions_controller.rb
+++ app/controllers/assessment/scribing_questions_controller.rb
@@ -7,11 +7,30 @@ class Assessment::ScribingQuestionsController < Assessment::QuestionsController
     qa = @assessment.question_assessments.new
     qa.question = @question.question
     qa.position = @assessment.questions.count
+    uploaded_file_object = params[:assessment_scribing_question][:document]
 
-    if params[:assessment_scribing_question][:document]
+    if uploaded_file_object && uploaded_file_object.content_type == 'application/pdf'   #special handling for PDF files
+      # write file out to filesystem
+      File.open(uploaded_file_object.original_filename, 'wb') do |file|
+        file.write(uploaded_file_object.read)
+      end
+      basename = uploaded_file_object.original_filename[0..-5]
+
+      #TODO: consider and fix security implications
+      # Puts together and runs the PDF to PNG conversion command
+      convert_cmd = "/usr/bin/pdftoppm -png -r 300 #{uploaded_file_object.original_filename} "
+      convert_cmd += "#{basename}"
+      `#{convert_cmd}`
+
+      file_save_success = true
+
+      # cleanup PDF file
+      File.delete(uploaded_file_object.original_filename)
+      
+    elsif uploaded_file_object
       file_upload = FileUpload.create({creator: current_user,
                                        owner: @question,
-                                       file: params[:assessment_scribing_question][:document]
+                                       file: uploaded_file_object
                                        })
       file_save_success = file_upload.save
     else
@@ -21,7 +40,7 @@ class Assessment::ScribingQuestionsController < Assessment::QuestionsController
     respond_to do |format|
       if @question.save && qa.save && file_save_success
         format.html { redirect_to url_for([@course, @assessment.as_assessment]),
-                      notice: 'Question has been added.' }
+                      notice: "Question has been added." }
         format.json { render json: @question, status: :created, location: @question }
       else
         format.html { render action: 'new' }
