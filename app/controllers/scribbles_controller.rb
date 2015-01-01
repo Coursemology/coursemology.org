02f47c024ff6edab4119e1c5fd9d6631020dc9b6
Brian Koh <kxmbrian@gmail.com>
2015-01-01 14:46:08 +0800

    Fix bug -- multiple scribbles was created per user unless user refreshes page after first scribble

--- app/controllers/scribbles_controller.rb
+++ app/controllers/scribbles_controller.rb
@@ -1,11 +1,18 @@
 class ScribblesController < ApplicationController
 
   def create
-    if scribble_params['id'] == ""
-      @scribble = Scribble.new(scribble_params)
-    else
+    if scribble_params['id'] != ""
       @scribble = Scribble.find(scribble_params['id'])
+    else
+      @scribble = Scribble.where({
+        std_course_id: scribble_params[:std_course_id],
+        scribing_answer_id: scribble_params[:scribing_answer_id]
+      }).first
+    end
+    if @scribble
       @scribble.update_attributes(scribble_params)
+    else
+      @scribble = Scribble.new(scribble_params)
     end
     @scribble.save
 
