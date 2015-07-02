ccd2c0c8c9a52f766684fd56958a0e2280b21909
Darius Foo <darius.foo.tw@gmail.com>
2015-07-02 18:28:41 +0800

    Highlighted keywords when viewing gradings

---
+++ app/helpers/assessment_helper.rb
@@ -0,0 +1,12 @@
+module AssessmentHelper
+  # Given a string of text and a list of keywords, highlights each
+  # keyword occurrence with a <span> tag.
+  def highlight_keywords(original, keywords)
+    highlighted = original.dup
+    keywords.each do |keyword|
+      highlighted.gsub!(Regexp.new("\\b#{keyword}\\b"), content_tag(:mark, '\0').html_safe)
+    end
+    highlighted
+  end
+end
+
