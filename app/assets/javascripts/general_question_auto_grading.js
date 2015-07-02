3ea64fba413008d4f08138fe0d8d72e0f8fdf2f5
Darius Foo <darius.foo.tw@gmail.com>
2015-07-02 18:41:57 +0800

    Added frontend score validation
    It is made to be less than the current max grade and positive.
    
    The validations go both ways, triggering when the max grade is changed,
    and when any score is changed. The latter is done because the score
    fields are dynamically created.

--- app/assets/javascripts/general_question_auto_grading.js
+++ app/assets/javascripts/general_question_auto_grading.js
@@ -1,8 +1,17 @@
 
+function setAutoGradingKeywordMaxScore() {
+    var maxValue = $('#assessment_general_question_max_grade').val();
+    $('input[id^=assessment_general_question_auto_grading_keyword_options_attributes][id$=score]')
+        .attr({ 'max' : maxValue });
+}
+
 $(function () {
     $('input[name=assessment_general_question\\[auto_grading_type\\]]').change(function () {
         $('.auto-grading').removeClass('in');
         var autoGradingType = $(this).val();
         $('#auto-grading-' + autoGradingType).addClass('in');
     });
+
+    $('#assessment_general_question_max_grade')
+        .change(setAutoGradingKeywordMaxScore);
 });
