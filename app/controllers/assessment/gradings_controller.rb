80d8672ae88c49fc8097b77287684030a68cc14d
Darius Foo <darius.foo.tw@gmail.com>
2015-07-09 18:39:16 +0800

    Fixed invalid uses of auto_graded?
    (on question types other than coding and general)

--- app/controllers/assessment/gradings_controller.rb
+++ app/controllers/assessment/gradings_controller.rb
@@ -29,23 +29,21 @@ class Assessment::GradingsController < ApplicationController
       qn = ans.question
       @summary[:qn_ans][qn.id][:ans] = ans
 
+      grade = 0
       #suggest grading for auto grading question
-      if qn.auto_graded?
-        if qn.is_a?(Assessment::CodingQuestion)
-          results = ans.result_hash["eval"]
-          evals = results ? results.select { |r| r }.length : 0
-          tests = qn.data_hash["eval"].length
-          tests = tests == 0 ? 1 : tests
-          grade = (qn.max_grade * evals / tests).to_i
-        elsif qn.is_a?(Assessment::GeneralQuestion)
-          # Check saved auto-grading options and suggest a grade
-          grade = 0
-          case qn.auto_grading_type
-          when :exact
-            grade = auto_grading_exact_grade(qn, ans.content)
-          when :keyword
-            grade = auto_grading_keyword_grade(qn, ans.content)
-          end
+      if qn.is_a?(Assessment::CodingQuestion) && qn.auto_graded?
+        results = ans.result_hash["eval"]
+        evals = results ? results.select { |r| r }.length : 0
+        tests = qn.data_hash["eval"].length
+        tests = tests == 0 ? 1 : tests
+        grade = (qn.max_grade * evals / tests).to_i
+      elsif qn.is_a?(Assessment::GeneralQuestion) && qn.auto_graded?
+        # Check saved auto-grading options and suggest a grade
+        case qn.auto_grading_type
+        when :exact
+          grade = auto_grading_exact_grade(qn, ans.content)
+        when :keyword
+          grade = auto_grading_keyword_grade(qn, ans.content)
         end
       end
       ag = ans.build_answer_grading
