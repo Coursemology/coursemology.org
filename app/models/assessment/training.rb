1467bc811575bef6717bd2370211a3600b4470f7
WANG QIANG <rovingbreeze@gmail.com>
2015-07-14 12:54:41 +0800

    Validates bonus cutoff time and provide proper hints

--- app/models/assessment/training.rb
+++ app/models/assessment/training.rb
@@ -13,8 +13,9 @@ class Assessment::Training < ActiveRecord::Base
   attr_accessible :dependent_on, :dependent_on_attributes
 
   validates_presence_of :title, :exp, :open_at
+  validates_numericality_of :bonus_exp
 
-  validates_with DateValidator, fields: [:open_at, :bonus_cutoff_at]
+  validate :bonus_cutoff_time_must_be_valid
 
   def full_title
     "#{I18n.t('Assessment.Training')} : #{self.title}"
@@ -22,5 +23,15 @@ class Assessment::Training < ActiveRecord::Base
 
   def get_path
     course_assessment_training_path(self.course, self)
+  end
+
+  private
+
+  def bonus_cutoff_time_must_be_valid
+    return if !bonus_exp || bonus_exp == 0
+
+    unless bonus_cutoff_at && bonus_cutoff_at > open_at
+      errors[:bonus_cutoff_at] << "must be after open time"
+    end
   end
 end
