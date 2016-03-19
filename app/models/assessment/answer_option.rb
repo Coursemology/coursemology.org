9e21a96b1cbf49059bbb003d4c8bafe9458b7842
WANG QIANG <rovingbreeze@gmail.com>
2016-03-19 10:18:35 +0800

    Answer option should belongs to MCQ answer

--- app/models/assessment/answer_option.rb
+++ app/models/assessment/answer_option.rb
@@ -3,9 +3,6 @@ class Assessment::AnswerOption < ActiveRecord::Base
 
   attr_accessible :option_id, :answer_id
 
-  belongs_to  :answer, class_name: Assessment::Answer
-  belongs_to  :option, class_name: Assessment::McqOption
-
-
-
+  belongs_to  :answer, class_name: Assessment::McqAnswer.name
+  belongs_to  :option, class_name: Assessment::McqOption.name
 end
