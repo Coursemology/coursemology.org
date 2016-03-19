c901e4b2457a1e73e18e1eb8a33d45fb23093071
WANG QIANG <rovingbreeze@gmail.com>
2016-03-19 16:31:39 +0800

    Do not specify type in remove column

--- db/migrate/20160319042230_add_uniq_index_to_user_courses.rb
+++ db/migrate/20160319042230_add_uniq_index_to_user_courses.rb
@@ -1,6 +1,6 @@
 class AddUniqIndexToUserCourses < ActiveRecord::Migration
   def change
     add_index :user_courses, [:user_id, :course_id], unique: true
-    remove_column :user_courses, :deleted_at, :time
+    remove_column :user_courses, :deleted_at
   end
 end
