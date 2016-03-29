893862353555f19f4630ea8f956a0c66febd7870
WANG QIANG <rovingbreeze@gmail.com>
2016-03-29 14:27:58 +0800

    Set default secrets for development/test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Coursemology::Application.config.secret_token = ENV['APP_SECRET']
+Coursemology::Application.config.secret_token =
+    if Rails.env.development? or Rails.env.test?
+      '6646d49dfcbde9fd877fe9317c14d417a6d433e36b1694441029109af312b3dde6356f47b8b422389cabb8e9e65ac1c75ea8ebfa60b46b8708ff9945fa3f58e7'
+    else
+      ENV['APP_SECRET']
+    end
