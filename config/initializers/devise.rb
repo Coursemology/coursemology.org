3f9ef802b1140a0668f4fd4d5f9027a627202132
WANG QIANG <rovingbreeze@gmail.com>
2017-04-03 14:24:17 +0800

    Rename APP_ID to FB_APP_ID

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -206,7 +206,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, ENV['APP_ID'], ENV['SECRET_KEY']
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET_KEY']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
